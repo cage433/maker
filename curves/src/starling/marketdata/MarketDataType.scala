fa593f243ef84e45838ae00f127d0ab686a9c354
stacy.curl <stacy.curl@trafigura.com>
2011-06-08 17:26:53 +0100

    Fixing bug due to 'keyish' field in PriceFixingsHistoryDataType
    exchange needs to be a key field in order to see Libor like market data, but exchange isn't always available
    
    So I've change the code to cease expecting all key fields to be available (at least when applying the override rule).
    
    Perhaps a better fix would be to define a new type of field that behaves like a key when present.

--- curves/src/starling/marketdata/MarketDataType.scala
+++ curves/src/starling/marketdata/MarketDataType.scala
@@ -12,7 +12,7 @@ trait MarketDataType {
 
   val name: String = getClass.getName.substring(getClass.getName.lastIndexOf(".") + 1).stripSuffix("DataType$")
   def keyFields:Set[Field]
-  def valueFields:Set[Field]
+  def valueFields:Set[Field] // TODO [08 Jun 2011] Shouldn't valueFields be everything other than the keyFields ?
   def createKey(values:Map[Field,Any]):MarketDataKey
   def createValue(values:List[Map[Field,Any]]):dataType
   override val toString = name
@@ -23,6 +23,8 @@ trait MarketDataType {
 
   //The initial state to use in the market data viewer for this type of market data
   val initialPivotState:PivotFieldsState
+
+  def splitByFieldType[T](map: Map[Field, T]) = map.filterKeys(keyFields) → map.filterKeys(f => !keyFields.contains(f))
 }
 
 object MarketDataTypes {
