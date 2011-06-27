10dea1bdc34bcf53b7bb254e762a0c4a5cc1b17a
Alex McGuire <alex.mcguire@trafigura.com>
2011-06-27 09:04:45 +0100

    Make ImportDataTask do a snapshot

--- services/src/starling/services/ImportMarketDataTask.scala
+++ services/src/starling/services/ImportMarketDataTask.scala
@@ -3,8 +3,8 @@ package starling.services
 import starling.daterange.Day
 import starling.db.MarketDataStore
 import starling.gui.api.{MarketDataSelection, PricingGroup}
-
+import org.scalacheck.Prop.True
 
 class ImportMarketDataTask(marketDataStore: MarketDataStore, pricingGroup: PricingGroup) extends ScheduledTask {
-  def execute(observationDay: Day) = marketDataStore.importData(MarketDataSelection(Some(pricingGroup)), observationDay)
+  def execute(observationDay: Day) = marketDataStore.snapshot(MarketDataSelection(Some(pricingGroup)), true, observationDay)
 }
