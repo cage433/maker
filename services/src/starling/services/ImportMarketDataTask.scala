323b5620853cd622cbc70ceef4c969e8da2982d4
Louis Botterill <louis.botterill@trafigura.com>
2011-06-28 17:01:12 +0100

    fix legacy maven builds to keep team city happy

--- services/src/starling/services/ImportMarketDataTask.scala
+++ services/src/starling/services/ImportMarketDataTask.scala
@@ -3,7 +3,6 @@ package starling.services
 import starling.daterange.Day
 import starling.db.MarketDataStore
 import starling.gui.api.{MarketDataSelection, PricingGroup}
-import org.scalacheck.Prop.True
 
 class ImportMarketDataTask(marketDataStore: MarketDataStore, pricingGroup: PricingGroup) extends ScheduledTask {
   def execute(observationDay: Day) = marketDataStore.snapshot(MarketDataSelection(Some(pricingGroup)), true, observationDay)
