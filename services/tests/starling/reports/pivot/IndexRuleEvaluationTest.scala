43bcc8fd79f85a9a31ca45c8c38806040909e1f7
david.corcoran <David.Corcoran@trafigura.com>
2011-06-30 15:45:07 +0100

    fixed typo in test

--- services/tests/starling/reports/pivot/IndexRuleEvaluationTest.scala
+++ services/tests/starling/reports/pivot/IndexRuleEvaluationTest.scala
@@ -31,6 +31,6 @@ class IndexRuleEvaluationTest extends JonTestEnv {
 
     val rows = IndexRuleEvaluation.rows(index, period, NonCommonPricingRule, Some(2), env)
     assertTrue(wti.observationDays(period).forall(day => rows._1.find(r => r.market == wti.market && r.day == day && r.observed == wti.observedPeriod(day)) isDefined))
-    assertTrue(brent.observationDays(period).forall(day => rows._1.find(r => r.market == brent.market && r.day == day && r.observed == wti.observedPeriod(day)) isDefined))
+    assertTrue(brent.observationDays(period).forall(day => rows._1.find(r => r.market == brent.market && r.day == day && r.observed == brent.observedPeriod(day)) isDefined))
   }
 }
