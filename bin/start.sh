e146f0c7fbb24af8fe40beab9a17814f419e8466
nick.darcy <nick.darcy@trafigura.com>
2011-07-22 17:42:19 +0100

    fix a merge problem

--- bin/start.sh
+++ bin/start.sh
@@ -34,7 +34,7 @@ else
     # I'm actually going to compile the whole project here because the server needs to serve up gui class files but
     # because it doesn't depend on it, they don't get compiled automatically when you start the server.
     echo "Compiling... [targets: $TARGETS]"
-    nohup java -jar sbt/sbt-launch.jar "project dev.launcher" $TARGETS >> logs/fullCompile.log 2>&1 &&
+    nohup java -jar sbt/sbt-launch.jar $TARGETS >> logs/fullCompile.log 2>&1 &&
 
     # Want to compile the booter module (and jar it). It is written in java and used in the exe generation. This should
     # probably be done in SBT.
