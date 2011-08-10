b7eae4992957eb885ec2213e00fbbd95843ea5e3
nick.darcy <nick.darcy@trafigura.com>
2011-08-10 17:20:15 +0100

    Fix a patch for the market data (spread markets).

---
+++ services/src/starling/schemaevolution/Patch113_FixPatch106.scala
@@ -0,0 +1,21 @@
+package starling.schemaevolution
+
+import system.Patch
+import starling.services.StarlingInit
+import starling.richdb.RichDB
+import starling.db.DBWriter
+
+
+class Patch113_FixPatch106 extends Patch {
+  protected def runPatch(starlingInit: StarlingInit, starling: RichDB, writer: DBWriter) {
+    writer.queryForUpdate("select * from marketdata where marketDAtaType = '<starling.curves.SpreadStdDevSurfaceDataType_-/>'") {
+      rs => {
+        val old = rs.getString("data")
+        val newS = old
+          .replaceAll("<first", "<front")
+          .replaceAll("<last", "<back")
+        rs.update(Map("data" -> newS))
+      }
+    }
+  }
+}
