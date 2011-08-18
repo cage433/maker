eecc78616f8d4d68ba2f5b6b08f0d27a87ae153a
nick.darcy <nick.darcy@trafigura.com>
2011-08-18 13:40:23 +0100

    dev launcher depends on the gui so we don't need this any more.

--- bin/start.sh
+++ bin/start.sh
@@ -31,11 +31,6 @@ else
 
     mkdir -p booter/out/
 
-    # I'm actually going to compile the whole project here because the server needs to serve up gui class files but
-    # because it doesn't depend on it, they don't get compiled automatically when you start the server.
-    echo "Compiling... "
-    nohup java -Dsbt.ivy.home=.ivy -Dhttp.nonProxyHosts=nexus.global.trafigura.com -jar sbt/sbt-launch.jar compile >> logs/fullCompile.log 2>&1 &&
-
     # Want to compile the booter module (and jar it). It is written in java and used in the exe generation. This should
     # probably be done in SBT.
     echo "Compile booter"
