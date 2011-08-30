f9776b2ef9a3aa75a5b5fea25870b8ee26025c04
nick.darcy <nick.darcy@trafigura.com>
2011-08-30 14:26:30 +0100

    Spread periods now obey user month formatting.

--- pivot.utils/src/starling/pivot/utils/PeriodPivotFormatter.scala
+++ pivot.utils/src/starling/pivot/utils/PeriodPivotFormatter.scala
@@ -2,7 +2,7 @@ package starling.pivot.utils
 
 import starling.pivot._
 import starling.pivot.MonthFormat._
-import starling.daterange.{DateRange, DateRangePeriod, Month}
+import starling.daterange.{SpreadPeriod, DateRange, DateRangePeriod, Month}
 
 object PeriodPivotFormatter extends PivotFormatter {
   def format(value:Any, formatInfo:ExtraFormatInfo) = {
@@ -29,6 +29,11 @@ object PeriodPivotFormatter extends PivotFormatter {
     value match {
       case DateRangePeriod(period) => match2(period)
       case dateRange:DateRange => match2(dateRange)
+      case sp@SpreadPeriod(dr1, dr2) => {
+        val t1 = match2(dr1)
+        val t2 = match2(dr2)
+        t1.copy(value = sp, text = (t1.text + "/" + t2.text))
+      }
       case other => DefaultPivotFormatter.format(other, formatInfo)
     }
   }
