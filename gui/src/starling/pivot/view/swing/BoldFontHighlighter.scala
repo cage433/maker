14f2ff11dd9eb267f905d38feb03f5765d194533
nick.darcy <nick.darcy@trafigura.com>
2011-09-08 16:13:07 +0100

    Ensure the refresh animation looks good when the refresh happens on a total or other non white colours.

--- gui/src/starling/pivot/view/swing/BoldFontHighlighter.scala
+++ gui/src/starling/pivot/view/swing/BoldFontHighlighter.scala
@@ -3,6 +3,7 @@ package starling.pivot.view.swing
 import collection.mutable.{HashMap,HashSet}
 import java.awt.Component
 import org.jdesktop.swingx.decorator.{ColorHighlighter, HighlightPredicate, ComponentAdapter, AbstractHighlighter}
+import starling.browser.common.RichColour._
 
 class BoldFontHighlighter(predicate:HighlightPredicate) extends AbstractHighlighter(predicate) {
   def doHighlight(component:Component, adapter:ComponentAdapter) = {
@@ -15,7 +16,9 @@ class UpdatingBackgroundColourHighlighter(predicate:HighlightPredicate, map:Hash
   def doHighlight(component:Component, adapter:ComponentAdapter) = {
     val index = (adapter.row,adapter.column)
     if (map.keySet.contains(index)) {
-      component.setBackground(map(index).currentColour)
+      val refreshedCell = map(index)
+      val bg = component.getBackground.blend(refreshedCell.currentColour, 1.0f - (refreshedCell.currentColour.getAlpha / 255.0f))
+      component.setBackground(bg)
     }
     component
   }
