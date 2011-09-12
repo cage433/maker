fe7d5b657eeb6a591e33730626fdc4ea511d5d4c
Alex McGuire <alex.mcguire@trafigura.com>
2011-09-12 11:34:30 +0100

    Fixed intellij build

--- bin/ivy.sh
+++
@@ -1,1 +0,0 @@
-java  -Dhttp.proxyHost=localhost -Dhttp.proxyPort=3128 -Dhttp.nonProxyHosts=nexus.global.trafigura.com -jar /usr/share/java/ivy.jar -settings ../ivysettings.xml -retrieve "lib_managed/[artifact]-[type]-[revision].[ext]" -sync
