23160ac77b5c2f9e4db365acb4b9c1c1e1bbadda
Alex McGuire <alex.mcguire@trafigura.com>
2011-09-19 09:25:35 +0100

    Add reports to modules passed to the client

--- services/src/starling/http/WebStartServlet.scala
+++ services/src/starling/http/WebStartServlet.scala
@@ -23,7 +23,7 @@ object GUICode {
 
   // The order of this list matters. It is the order things are attempted to be loaded so ensure it is optimised.
   val modules = List("daterange", "quantity", "utils", "auth", "bouncyrmi", "gui", "gui.api",
-    "pivot", "pivot.utils", "browser", "browser.service", "fc2.api", "launcher", "manager", "singleclasspathmanager")
+    "pivot", "pivot.utils", "browser", "browser.service", "fc2.api", "launcher", "manager", "singleclasspathmanager", "reports")
 
   val libJarNames = Map(
     "scala-library" -> scalaLibraryJar,
