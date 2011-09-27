36d75f2ae02a9f549077185b7482271e4fc39765
Alex McGuire <alex.mcguire@trafigura.com>
2011-09-27 10:48:57 +0100

    Revert "Documented Server, Brompton -- DM"
    This reverts commit 32d9d7043279dc4d2023a2c223ef99e0bc2b17fd.

--- startserver/src/starling/startserver/Server.scala
+++ startserver/src/starling/startserver/Server.scala
@@ -16,28 +16,14 @@ import starling.metals.MetalsBromptonActivator
 
 
 /**
- * The Server object provides a singleton implementation for starting the Starling Server either as a Scala application
- * (using its main(..) method) or programmatically by invoking its run() method.
- * 
- * @see run
- * @documented
+ * Starts the server, without osgi
  */
 object Server {
 
-  /**
-   * Invokes this instance's run method.
-   * @param args Ignored.
-   * @documented
-   */
   def main(args:Array[String]) {
     run()
   }
 
-  /**
-   * Runs the Starling server.  The log4j configuration is read from the util/resources/log4j.properties. The properties
-   * are loaded from the "props.conf" file.  The application's process ID is written to the "pid.txt" file.  If for any
-   * reason the server cannot be started (e.g. bad data in the database), a RuntimeException should be thrown.
-   */
   def run() {
     System.setProperty("log4j.configuration", "utils/resources/log4j.properties")
     PropsHelper.writeDefaults
@@ -60,11 +46,6 @@ object Server {
     }
   }
 
-  /**
-   * Writes the application's process ID (from the ManagementFactory's runtime MX bean's name) to the "pid.txt" file.
-   * This file should be deleted on shutdown.
-   * @documented
-   */
   def writePIDFile() {
     val processName = ManagementFactory.getRuntimeMXBean.getName
     val pid = processName.subSequence(0, processName.indexOf("@")).toString
@@ -79,4 +60,5 @@ object Server {
       override def run() = if (file.exists) file.delete
     })
   }
+
 }
