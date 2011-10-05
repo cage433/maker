848fb2365d9e422ebb3d591f345c599c7022040f
david.corcoran <David.Corcoran@trafigura.com>
2011-10-05 15:45:42 +0100

    reload if deal isn't recognised

--- databases/src/starling/eai/EAIDealBookMapping.scala
+++ databases/src/starling/eai/EAIDealBookMapping.scala
@@ -2,14 +2,33 @@ package starling.eai
 
 import starling.db.DB
 import starling.dbx.QueryBuilder._
+import concurrent.stm._
+import starling.utils.Log
 
 class EAIDealBookMapping(db: DB) {
-  private lazy val dealToBook = db.queryWithResult(select("*") from ("tblBookDealsMap")) {
-    rs => (rs.getInt("DealID") -> rs.getInt("BookID"))
-  }.toMap
+  class Mapping {
+    private lazy val dealToBook = db.queryWithResult(select("*") from ("tblBookDealsMap")) {
+      rs => (rs.getInt("DealID") -> rs.getInt("BookID"))
+    }.toMap
 
-  def book(deal: Int): Int = dealToBook.get(deal) match {
-    case Some(book) => book
-    case None => throw new Exception("Not a recognised deal id: " + deal)
+    def book(deal: Int): Option[Int] = dealToBook.get(deal)
+  }
+
+  private val mapping = Ref(new Mapping)
+
+  def book(deal: Int): Int = atomic {
+    implicit txn => {
+      mapping().book(deal) match {
+        case Some(book) => book
+        case None => {
+          Log.info("Deal id " + deal + " not found, reloading deals mapping.")
+          mapping() = new Mapping
+          mapping().book(deal) match {
+            case Some(book) => book
+            case None => throw new Exception("Not a recognised deal id: " + deal)
+          }
+        }
+      }
+    }
   }
 }
