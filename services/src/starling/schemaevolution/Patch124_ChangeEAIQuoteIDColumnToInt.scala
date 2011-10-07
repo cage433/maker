ba7e8165e87bb1eaa40c71bb6e48532c0520df06
stacy.curl <stacy.curl@trafigura.com>
2011-10-07 11:15:46 +0100

    Fixing ordering of patches

--- services/src/starling/schemaevolution/Patch124_ChangeEAIQuoteIDColumnToInt.scala
+++ services/src/starling/schemaevolution/Patch124_ChangeEAIQuoteIDColumnToInt.scala
@@ -6,7 +6,7 @@ import starling.richdb.RichDB
 import starling.db.DBWriter
 import starling.dbx.QueryBuilder._
 
-class Patch123_ChangeEAIQuoteIDColumnToInt extends Patch {
+class Patch124_ChangeEAIQuoteIDColumnToInt extends Patch {
   protected def runPatch(starlingInit: StarlingInit, starling: RichDB, writer: DBWriter) {
     starling.inTransaction{
       writer => writer.update("ALTER TABLE Markets ALTER COLUMN EAIQuoteID INT")
