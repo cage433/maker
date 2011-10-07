df1ab73558219cf3a86259589daedf209fc1d531
Thomas Rynne <thomas.rynne@trafigura.com>
2011-10-07 10:00:50 +0100

    Rabbit Event Viewer: Fixes bug when filtering on day and changes the sorting and timestamp format

--- rabbit.event.viewer.service/src/starling/rabbiteventviewer/internal/RabbitEventViewerServiceImpl.scala
+++ rabbit.event.viewer.service/src/starling/rabbiteventviewer/internal/RabbitEventViewerServiceImpl.scala
@@ -1,10 +1,12 @@
 package starling.rabbiteventviewer.internal
 
 import starling.rabbiteventviewer.api.RabbitEventViewerService
-import starling.dbx.{From, RealTable}
 import starling.pivot.model.PivotTableModel
 import starling.pivot._
 import starling.rmi.{DefaultRabbitEventDatabase, RabbitEventDatabase}
+import starling.dbx.{Clause, From, RealTable}
+import starling.daterange.{Timestamp, Day}
+import utils.{TimestampPivotFormatter, TimeOnlyTimestampPivotFormatter}
 
 class RabbitEventViewerServiceImpl(eventDatabase:RabbitEventDatabase) extends RabbitEventViewerService {
   def rabbitEvents(pivotFieldParams:PivotFieldParams, latestEvent:Long) = {
@@ -23,15 +25,44 @@ class RabbitEventViewerServiceImpl(eventDatabase:RabbitEventDatabase) extends Ra
 
     val columns = {
       List(("Event Fields", List(
-        new LongColumnDefinition(starlingID, "starlingID", table),
+        new LongColumnDefinition(starlingID, "starlingID", table) {
+          override def fieldDetails:FieldDetails = new FieldDetails(name) {
+            override def comparator = new Ordering[Any]() {
+              def compare(x: Any, y: Any) = y.asInstanceOf[Long].compare(x.asInstanceOf[Long])
+            }
+          }
+        },
         StringColumnDefinition(verb, "verb", table),
         StringColumnDefinition(subject, "subject", table),
         StringColumnDefinition(id, "id", table),
         StringColumnDefinition(source, "source", table),
-        new TimestampColumnDefinition(timestamp, "timestamp", table),
-        new TimestampColumnDefinition(starlingTimestamp, "starlingTimestamp", table),
+        new TimestampColumnDefinition(timestamp, "timestamp", table) {
+          override def fieldDetails:FieldDetails = new FieldDetails(name) {
+            override def formatter = TimeOnlyTimestampPivotFormatter
+            override def comparator = new Ordering[Any]() {
+              def compare(x: Any, y: Any) = y.asInstanceOf[Timestamp].compare(x.asInstanceOf[Timestamp])
+            }
+          }
+        },
+        new TimestampColumnDefinition(starlingTimestamp, "starlingTimestamp", table) {
+          override def fieldDetails:FieldDetails = new FieldDetails(name) {
+            override def formatter = TimestampPivotFormatter
+            override def comparator = new Ordering[Any]() {
+              def compare(x: Any, y: Any) = y.asInstanceOf[Timestamp].compare(x.asInstanceOf[Timestamp])
+            }
+          }
+        },
         new DayColumnDefinition("Day", table) {
           override val fullSqlName = "timestamp"
+          override def filterClauses(values:Set[Any]):List[Clause] = values.toList.map { value => {
+            val day = value.asInstanceOf[NullableDay].day
+            (starling.dbx.Field("timestamp") gt day.toSqlDate) and (starling.dbx.Field("timestamp") lte day.nextDay.toSqlDate)
+          } }
+          override def fieldDetails:FieldDetails = new FieldDetails(name) {
+            override def comparator = new Ordering[Any]() {
+              def compare(x: Any, y: Any) = y.asInstanceOf[NullableDay].day.compare(x.asInstanceOf[NullableDay].day)
+            }
+          }
         },
         StringColumnDefinition(host, "host", table),
         new IntColumnDefinition(pid, "pid", table),
