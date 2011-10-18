440c3c64981f8f0e26313c3133f1901eb7b7868d
Louis Botterill <louis.botterill@trafigura.com>
2011-10-18 17:27:58 +0100

    new C&I valuation report

---
+++ reports.impl/src/starling/reports/impl/pivot/CostAndIncomeValuationReport.scala
@@ -0,0 +1,13 @@
+package starling.reports.impl.pivot
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: louis
+ * Date: 18/10/11
+ * Time: 15:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CostAndIncomeValuationReport {
+
+}
