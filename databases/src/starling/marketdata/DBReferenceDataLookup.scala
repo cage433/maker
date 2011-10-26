49a021e5f8ab2e0328bd686130cf5d14c582b5ef
Alex McGuire <alex.mcguire@trafigura.com>
2011-10-26 18:09:20 +0100

    Fixed null pointer bug

--- databases/src/starling/marketdata/DBReferenceDataLookup.scala
+++ databases/src/starling/marketdata/DBReferenceDataLookup.scala
@@ -5,17 +5,16 @@ import starling.utils.ImplicitConversions._
 
 
 case class DBReferenceDataLookup(neptuneDB: RichDB) extends ReferenceDataLookup {
-  def name = "Neptune DB Reference Data Lookup"
+  private val countryToAreaLT = neptuneDB.lookupTable("live.location_area_link", "country_code", "area_code")
+    .mapKeys(NeptuneCountryCode(_)).mapValues(AreaCode(_))
 
+  def name = "Neptune DB Reference Data Lookup"
   val areas = neptuneDB.lookupTable("live.geographical_location", "code", "description", AreaCode.apply, Area.apply)
   val incoterms = neptuneDB.lookupTable("live.delivery_term", "code", "description", IncotermCode.apply, Incoterm.apply)
   val grades = neptuneDB.lookupTable("live.category", "code", "description", GradeCode.apply, Grade.apply)
 
   val contractLocations = neptuneDB.lookupTable("live.contract_location", "code", "description",
     ContractualLocationCode.apply, ContractualLocation.apply)
-
-  private val countryToAreaLT = neptuneDB.lookupTable("live.location_area_link", "country_code", "area_code")
-    .mapKeys(NeptuneCountryCode(_)).mapValues(AreaCode(_))
 
   val countries = neptuneDB.lookupTable[NeptuneCountryCode, NeptuneCountry]("live.country", "code", "name",
     NeptuneCountryCode.apply, (code, name) => NeptuneCountry(code, name, countryToAreaLT.get(code).flatMap(areas.get)))
