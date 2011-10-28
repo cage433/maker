da779b4d9597f3d6ca5019b651db6c725db84951
nick.darcy <nick.darcy@trafigura.com>
2011-10-28 15:07:37 +0100

    team city needs to publish

--- teamcity.sh
+++ teamcity.sh
@@ -16,5 +16,5 @@ echo "------------------------------------------------------"
 echo
 echo "##teamcity[progressStart 'compile']"
 #java -Xmx4048M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=650m -jar -Dsbt.ivy.home=.ivy -Dhttp.nonProxyHosts=nexus.global.trafigura.com sbt/sbt-launch-0.10.jar 'project devLauncher' "test"
-java -Xmx4048M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=650m -jar -Dsbt.ivy.home=.ivy -Dsbt.log.noformat=true -Dhttp.nonProxyHosts=nexus.global.trafigura.com sbt/sbt-launch-0.11.0.jar ";clean ;test ;project doc-all; doc"
+java -Xmx4048M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=650m -jar -Dsbt.ivy.home=.ivy -Dsbt.log.noformat=true -Dhttp.nonProxyHosts=nexus.global.trafigura.com sbt/sbt-launch-0.11.0.jar ";clean ;test ;publish ;project doc-all; doc"
 
