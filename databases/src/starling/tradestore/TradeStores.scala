70e63744cc6c745fb84dc03db2b2c5e0a5795388
david.corcoran <David.Corcoran@trafigura.com>
2011-11-16 17:45:31 +0000

    don't include trades for 'today' from eai in reports

--- databases/src/starling/tradestore/TradeStores.scala
+++ databases/src/starling/tradestore/TradeStores.scala
@@ -138,7 +138,14 @@ case class TradeStores(
     }
 
     val deskTradeSetsAndTimestamps = tradeSelection.deskAndTimestamp.toList.flatMap {
-      case (desk, ts) => deskDefinitions(desk).tradeSets(tradeSelection.tradePredicate).map((_, ts.timestamp))
+      case (desk, ts) => {
+        val predicate: List[(Field, Selection)] = tradeSelection.deskAndTimestamp.toList.flatMap {
+          case (desk, tradeTimestamp) => {
+            List((Field("Trade Day"), LessThanOrEqualSelection(tradeTimestamp.closeDay)))
+          }
+        }
+        deskDefinitions(desk).tradeSets(TradePredicate(predicate ::: tradeSelection.tradePredicate.filter)).map((_, ts.timestamp))
+      }
     }
 
     intradayTradesetsAndTimestamps ::: deskTradeSetsAndTimestamps
