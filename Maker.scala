e3d65c3246a7f781054ac23edeca2136a711a4ab
LouisB <chillipower.uk@gmail.com>
2011-12-03 17:41:16 +0000

    add second module, utils with dependency on manager

--- Maker.scala
+++ Maker.scala
@@ -1,11 +1,23 @@
 import maker.project.Project
 import java.io.File
 
+val libRoot = new File("lib_managed")
+
 val managerRoot = new File("manager")
 val manager = new Project( 
   "manager",
   managerRoot,
   List(new File(managerRoot, "src")),
   List(new File(managerRoot, "tests")),
-  List(new File("lib"), new File("lib_managed"))
+  List(new File("lib"), libRoot)
 )
+
+val utilsRoot = new File("utils") 
+val utils = new Project(
+  "utils",
+  utilsRoot,
+  List(new File(utilsRoot, "src")),
+  List(new File(utilsRoot, "tests")),
+  List(new File("lib"), libRoot)
+) dependsOn (manager)
+
