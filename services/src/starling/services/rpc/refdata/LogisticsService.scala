3727c11b726a2338416aa1d7574ec74de69d70a2
Louis Botterill <louis.botterill@trafigura.com>
2011-12-06 16:25:29 +0000

    tidy

--- services/src/starling/services/rpc/refdata/LogisticsService.scala
+++ services/src/starling/services/rpc/refdata/LogisticsService.scala
@@ -3,6 +3,7 @@ package starling.services.rpc.refdata
 import starling.utils.Log
 import com.trafigura.services.Logistics.LogisticsServiceApi
 import starling.titan.{TitanLogisticsServices, TitanServices}
+import net.liftweb.json._
 
 class LogisticsService(titanServices: TitanServices, logisticsServices : TitanLogisticsServices)
   extends LogisticsServiceApi with Log {
@@ -13,9 +14,6 @@ class LogisticsService(titanServices: TitanServices, logisticsServices : TitanLo
     val quotas = response.associatedQuota
 
     val json = inventory.map(_.toJson()) ::: quotas.map(_.toJson())
-
-    import net.liftweb.json._
-    import net.liftweb.json.pretty
 
     val r = json.map(s => pretty(render(parse(s.toString))))
     r
