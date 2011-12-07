a0feab156af75b9d14ea6a7f7d705ffa637461a1
stacy.curl <stacy.curl@trafigura.com>
2011-12-07 11:19:34 +0000

    Improved logging of erroneous methods in loopyxl

--- loopyxl/src/starling/loopyxl/MethodOwner.scala
+++ loopyxl/src/starling/loopyxl/MethodOwner.scala
@@ -3,16 +3,18 @@ package starling.loopyxl
 import java.lang.reflect.Method
 import scala.collection.JavaConversions._
 import starling.utils.{VarLogger, Log}
+import starling.utils.ImplicitConversions._
 
 import collection.immutable.List
 
 
-class MethodOwner(owner: AnyRef, logger : VarLogger = Log) {
+class MethodOwner(owner: AnyRef, log : VarLogger = Log) {
   def dynamicMethods : List[DynamicMethod] = {
     val methods = annotatedMethods.map(method => new DynamicMethod(method, owner));
 
-    val unmarshallable = methods.flatMap(_.unmarshallable).distinct
-    unmarshallable.foreach(logInvalid(_))
+    val unmarshallable = methods.toMapWithValues(_.unmarshallable).mapValues(_.distinct).filterValues(_.nonEmpty)
+    unmarshallable.foreach(mc => log.error("ExcelMethod is not valid: %s, because it cannot contain classes: %s"
+      % (mc._1.name, mc._2.map(_.getSimpleName).mkString(", "))))
 
     methods.filter(_.isValid)
   }
@@ -20,8 +22,8 @@ class MethodOwner(owner: AnyRef, logger : VarLogger = Log) {
   private def annotatedMethods : List[Method] = owner.getClass.getMethods.filter(hasAnnotation(_)).toList
   private def hasAnnotation(method: Method) = method.getAnnotation(classOf[ExcelMethod]) != null
   private def onlyHasVarargsAtEnd(method: Method) = !method.getParameterTypes.init.exists(isSequence(_))
-  private def logInvalid(method : DynamicMethod) = logger.error("ExcelMethod is not valid: " + method.name)
-  private def logInvalid(aClass : Class[_]) = logger.error("ExcelMethod cannot contain class: " + aClass.getSimpleName)
+  private def logInvalid(method : DynamicMethod) = log.error("ExcelMethod is not valid: " + method.name)
+  private def logInvalid(aClass : Class[_]) = log.error("ExcelMethod cannot contain class: " + aClass.getSimpleName)
   private def isSequence(aClass: Class[_]) = classOf[Seq[_]].isAssignableFrom(aClass)
 }
 
