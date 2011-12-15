d9a1a2622361ca70c7aa5292e5fcb7e887b32c32
nick.darcy <nick.darcy@trafigura.com>
2011-12-15 10:48:46 +0000

    Some of the bookmarks weren't being saved properly.

--- gui/src/starling/gui/pages/EmailsSentPage.scala
+++ gui/src/starling/gui/pages/EmailsSentPage.scala
@@ -5,12 +5,13 @@ import starling.gui.{LocalCacheKeys, StarlingIcons}
 import starling.browser.{ServerContext, LocalCache}
 import starling.services.EmailService
 import starling.daterange.Timestamp
+import starling.gui.osgi.StarlingBrowserBundle
 
 case class EmailsSentPage(pps: PivotPageState, latestEmail: Timestamp) extends AbstractPivotPage(pps) {
   type SC = EmailService
   def text = "Emails Sent"
   override def icon = StarlingIcons.im("/icons/16x16_mail.png")
-  def bundle = "StarlingServer"
+  def bundle = StarlingBrowserBundle.BundleName
   def createServerContext(sc:ServerContext) = sc.lookup(classOf[EmailService])
   def selfPage(pivotPageState:PivotPageState, edits:PivotEdits) = copy(pps = pivotPageState)
   def dataRequest(emailService: EmailService) = emailService.emailsSent(latestEmail, pps.pivotFieldParams)
