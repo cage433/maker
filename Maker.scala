7d000acbb101bb7c118627692f77581e6159d518
Alex McGuire <cage433@gmail.com>
2011-12-21 00:00:26 +0000

    Various maker stuff

---
+++ Maker.scala
@@ -0,0 +1,22 @@
+import maker.project.Project
+import java.io.File
+
+def project(name : String) = Project(
+  name, 
+  new File(name),
+  List(new File(name, "src")),
+  List(new File(name, "tests")),
+  List(new File(name, "lib_managed"), new File(name, "lib"), new File(name, "maker-lib"))
+)
+val manager = project("manager")
+val utils = project("utils") dependsOn manager
+val osgirun = Project(
+  "osgirun", 
+  new File("osgirun"),
+  List(new File("osgirun/src")),
+  List(new File("osgirun/tests")),
+  List(new File("osgirun/lib_managed"), new File("osgirun/lib"), new File("osgirun/osgi_jars"))
+)
+val booter = project("booter")
+val concurrent = project("concurrent") dependsOn utils
+val quantity = project("quantity") dependsOn utils
