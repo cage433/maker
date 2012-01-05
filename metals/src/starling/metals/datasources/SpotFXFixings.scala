60c77cb451da0ffa9fe42bcfed13249da5623246
stacy.curl <stacy.curl@trafigura.com>
2012-01-05 16:59:57 +0000

    Refactored SpotFXFixings

--- metals/src/starling/metals/datasources/SpotFXFixings.scala
+++ metals/src/starling/metals/datasources/SpotFXFixings.scala
@@ -14,31 +14,36 @@ import Level._
 import Pattern._
 import LIMService.TopRelation._
 import ObservationTimeOfDay._
+import starling.utils.ImplicitConversions._
 
 
 object SpotFXFixings {
   import UOM._;
   import Trafigura.Bloomberg._
 
-  val all = List(new SpotFXFixings("ECB", ECBPublicationTime, Spot, EUR, """ECB(\w+)""", ForeignExchange.Ecb),
-    new SpotFXFixings("LME", LMEClose, Close, USD, """TRAF\.LME\.(\w+)""", Currencies.LME, Currencies.Lme), CFETSSpotFXFixings)
+  val all = List(new SpotFXFixings("ECB", ECBPublicationTime, Spot, EUR, "ECB", ForeignExchange.Ecb),
+    new SpotFXFixings("LME", LMEClose, Close, USD, "TRAF.LME.", Currencies.LME, Currencies.Lme), CFETSSpotFXFixings)
 }
 
-class SpotFXFixings(exchange: String, timeOfDay: ObservationTimeOfDay, level: Level, against: UOM, regex: String, nodes: LimNode*)
-  extends HierarchicalLimSource(nodes.toList, List(level)) {
+class SpotFXFixings(exchange: String, timeOfDay: ObservationTimeOfDay, level: Level, against: UOM, prefix: String, nodes: LimNode*)
+  extends LimSource(List(level)) {
 
-  type Relation = UOM
-  def relationExtractor = Extractor.regex(regex) { case List(UOM.Parse(currency)) => Some(currency) }
+  type Relation = Nothing
 
-  override def marketDataEntriesFrom(prices: List[Prices[UOM]]) = prices.map { case Prices(currency, priceByLevel, observationDay) =>
-    MarketDataEntry(observationDay.atTimeOfDay(timeOfDay), key(currency), value(priceByLevel(level), currency))
+  def description = nodes.map(node => "%s %s.* (%s)" % (node.name, prefix, level)).toList
+
+  override def marketDataEntriesFrom(connection: LIMConnection, start: Day, end: Day) = {
+    UOM.currencies.toMapWithValues { currency => connection.getPrices(prefix + currency, level, start, end) }
+      .mapNested { case (currency, observationDay, fixing) =>
+        MarketDataEntry(observationDay.atTimeOfDay(timeOfDay), key(currency), value(fixing, currency))
+      }.toList
   }
 
-  protected def key(currency: UOM): MarketDataKey = PriceFixingsHistoryDataKey(currency.toString, Some(exchange))
-
-  protected def value(price: Double, currency: UOM): MarketData = PriceFixingsHistoryData.create(
-    level, StoredFixingPeriod.tenor(Tenor.OneDay), (Quantity(price, currency / against)))
+  private def key(currency: UOM): MarketDataKey = PriceFixingsHistoryDataKey(currency.toString, Some(exchange))
+  private def value(price: Double, currency: UOM): MarketData = PriceFixingsHistoryData.create(
+      level, StoredFixingPeriod.tenor(Tenor.OneDay), (Quantity(price, currency / against)))
 }
+
 
 object CFETSSpotFXFixings extends LimSource(List(Close)) {
   type Relation = Nothing
