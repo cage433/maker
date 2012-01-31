ae4d6088d128ccf8715f8ffd39f7c09209da3139
Alex McGuire <alex.mcguire@trafigura.com>
2012-01-31 13:16:03 +0000

    Add missing dependency and function to write classpath

--- Starling.scala
+++ Starling.scala
@@ -54,3 +54,19 @@ val webservice = project("webservice") dependsOn (props, starlingApi)
 val startserver = project("startserver") dependsOn (reportsImpl, metals, starlingClient, webservice, rabbitEventViewerService)
 val launcher = project("launcher") dependsOn (startserver, booter)
 
+
+import java.io._
+
+def writeToFile(fileName : String, text : String){
+  val fstream = new FileWriter(fileName)
+  val out = new BufferedWriter(fstream)
+  out.write(text)
+  out.close()
+}
+
+def writeClasspath{
+  val cp = launcher.compilationClasspath
+  writeToFile("launcher-classpath.sh", "export STARLING_CLASSPATH=" + cp)
+}
+
+
