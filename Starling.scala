67fb1ab8c35e3d6badb2c8ff3ce08d1bdaf9fb89
Louis Botterill <louis.botterill@trafigura.com>
2012-04-13 15:59:58 +0100

    add ability to update titan db schemas from maker project (wrapper around dbdeploy for now) for convenience & dev efficiency

--- Starling.scala
+++ Starling.scala
@@ -14,7 +14,7 @@ import maker.os.Command
 import maker.os.Command._
 
 lazy val makerProps : Props = file("Maker.conf")
-lazy val unmanagedGlobalProperties : Properties = file("Starling.conf")
+lazy val unmanagedGlobalProperties : Properties = file("developer.conf")
 lazy val starlingProperties : Properties = file("props.conf")
 
 def project(name : String) = new Project(
@@ -362,15 +362,32 @@ def updateIvyFromProjectPom(project : Project) = {
   val args = List("ant", "-f", file(project.root, "antMakeIvy.xml").getAbsolutePath)
   val cmd = Command(Some(project.root), args : _*)
   val r = cmd.exec() match {
-    case (0, _) => println("Process completed")
-    case err @ (_, _) => println("Process failed: " + err)
+    case res @ (0, _) => println("Process completed"); res
+    case err @ (_, _) => println("Process failed: " + err); err
   }
   tmpFile.delete
   r
 }
 
+def updateTitanSchema(project : Option[Project] = None) {
+  val envName = Option(unmanagedGlobalProperties.getProperty("titan.env.name")).getOrElse{
+    throw new Exception("Missing env name, property = titan.env.name")
+  }
+  val scriptDir = file("../../bin/")
+  val script = file(scriptDir, "deploy2db")
+println("script = " + script.getAbsolutePath + ", exists = " + script.exists)
+  println("updating %s for env %s".format(project.map(_.name).getOrElse("None"), envName))
+  val args = "../../bin/" + script.getName :: ("-e" + envName) :: project.toList.map(p => ("-c" + p.name))
+  Command(Some(file(".").getAbsoluteFile), args : _*).exec() match {
+    case res @ (0, _) => println("Process completed"); res
+    case err @ (_, _) => println("Process failed: " + err); err
+  }
+}
+def updateAllTitanSchemas() = updateTitanSchema()
+
 case class RichProject(project : Project) {
-  def updateIvyFromPom() = updateIvyFromProjectPom(project) 
+  def updateIvyFromPom() = updateIvyFromProjectPom(project)
+  def updateSchema() = updateTitanSchema(Some(project))
 }
 object RichProject {
   implicit def toRichProject(project : Project) : RichProject = new RichProject(project)
