7ee68da6930fb9d610887de546cbe4101b8de514
Louis Botterill <louis.botterill@trafigura.com>
2012-04-23 11:20:48 +0100

    updates for CI build

--- build-all.scala
+++ build-all.scala
@@ -6,12 +6,21 @@ import maker.task.TaskFailed
 
 titanBuilder.clean
 titanBuilder.update
+
+/**
+ * invoicing generate some code on the fly
+* it's quite static so we could check it into Git, or 
+*   unwind the actual command line from maven and call that directly
+*/
+titanInvoicing.mvn("compile", "-PWebService")
+
 val r = titanBuilder.compile
 
 r.res match {
-  case Right(_) => {
+  case Right(r) => {
     println("Build OK")
     r.stats.foreach(println)
+    println(r)
     System.exit(0)
   }
   case Left(TaskFailed(_, reason)) => {
