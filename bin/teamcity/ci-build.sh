c814ad7940fe3fe80c4693ccb4bcd654f34e969a
Louis Botterill <louis.botterill@trafigura.com>
2012-04-23 14:05:16 +0100

    add some stats to maker teamcity script

--- bin/teamcity/ci-build.sh
+++ bin/teamcity/ci-build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-./maker/dist/bin/maker.sh -p maker/build-all.scala
+echo "Running teamcity.sh"
+echo "------------------------------------------------------"
+echo "pwd `pwd`"
+echo "date: `date`"
+java -version
+echo "uname: `uname -a`"
+echo "whoami: `whoami`"
+echo "CPUs"
+grep processor /proc/cpuinfo
+echo "Memory"
+free -m
+echo "------------------------------------------------------"
+echo
+echo "##teamcity[progressStart 'compile']"
+echo "version = $1"
 
+./maker/dist/bin/maker.sh -p maker/build-all.scala
