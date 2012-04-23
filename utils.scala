2cfdd7f6d909481bd0e8a0c97f4ba1966d30b69f
Louis Botterill <louis.botterill@trafigura.com>
2012-04-23 11:20:48 +0100

    updates for CI build

--- utils.scala
+++ utils.scala
@@ -1,6 +1,16 @@
 println("\n ** Loading build utils...\n")
 
 
+def runCmd(cmd : Command) : Either[(Int, String), String] =
+  cmd.exec() match {
+    case res @ (0, msg) => println("Process completed"); Right(msg)
+    case err @ (_, _) => println("Process failed: " + err); Left(err)
+    case _ => throw new Exception("Unhandled command result")
+  }
+
+def runMavenCmd(cwd : File, args : String*) =
+  runCmd(Command(Some(cwd), ("mvn" :: args.toList) : _*))
+
 def updateIvyFromProjectPom(project : Project) = {
   val antFileName = "antMakeIvy.xml"
   val antFile = file("maker", antFileName)
@@ -32,9 +42,16 @@ println("script = " + script.getAbsolutePath + ", exists = " + script.exists)
 }
 def updateAllTitanSchemas() = updateTitanSchema()
 
+/**
+ * litle bit of pimping of a standard Maker project,
+ *  add in some Titan / maven related utils and integration
+ */
 case class RichProject(project : Project) {
-  def updateIvyFromPom() = updateIvyFromProjectPom(project)
-  def updateSchema() = updateTitanSchema(Some(project))
+  def updateIvyFromPom = updateIvyFromProjectPom(project)
+  def updateSchema = updateTitanSchema(Some(project))
+  def mvnCompile = runMavenCmd(project.root, "compile")
+  def mvnInstall = runMavenCmd(project.root, "install")
+  def mvn(cmd : String, args : String*) = runMavenCmd(project.root, (cmd :: args.toList) : _*)
 }
 object RichProject {
   implicit def toRichProject(project : Project) : RichProject = new RichProject(project)
