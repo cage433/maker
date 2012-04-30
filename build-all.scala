7b7709e911f08dcfc6879b933a4976e11bb8cf9e
Louis Botterill <louis.botterill@trafigura.com>
2012-04-30 11:28:09 +0100

    get some titan unit tests working

--- build-all.scala
+++ build-all.scala
@@ -5,7 +5,11 @@ println("\n ** Loading build-all...\n")
 import maker.task.TaskFailed
 import maker.task.BuildResult
 import maker.task.ProjectAndTask
+import maker.task.Task
 import maker.task.tasks._
+
+def mkBuildResult(project : Project, task : Task) =
+  BuildResult(Right("OK (faked)"), Set(), ProjectAndTask(project, task))
 
 val buildResult = for {
   _ <- titanBuilder.clean
@@ -14,6 +18,7 @@ val buildResult = for {
           // less than ideal, but ivy update is currently unreliable - so return a fake success always ultil the issue is sorted... 
           // (this is ok-ish because the local  cache is cummulative so even if first run fails it's usually ok by second or third pass
           BuildResult(Right("OK, checking disabled as Nexus is unreliable at the moment"), Set(), ProjectAndTask(titanBuilder, UpdateTask))
+          mkBuildResult(titanBuilder, UpdateTask)
   }
   _ <- launcher.test
 
@@ -24,6 +29,13 @@ val buildResult = for {
  */
 //  _ <- titanInvoicing.mvn("compile", "-PWebService")  // currently problematic on teamcity agent
   _ <- titanBuilder.compile
+
+// titan unit tests / classpath and deps needs more work before it's fully integrated with a single build
+// so for now setup the titan components so the unit tests can be run, then run them explicitly
+  _ <- {
+         titanComponents.foreach(_.updateOnly("default", "test"))
+         mkBuildResult(starlingTitanDeps, UpdateTask)
+  }
   _ <- titanMurdoch.testOnly
   _ <- titanReferenceData.testOnly
   _ <- titanTradeService.testOnly
