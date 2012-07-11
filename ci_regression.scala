f6322c84cbc7eb85885f3d2188510156e3d56760
Louis Botterill <louis.botterill@trafigura.com>
2012-07-11 07:17:51 +0100

    maker bookmark tests should now work

--- ci_regression.scala
+++ ci_regression.scala
@@ -3,14 +3,18 @@ import Utils._
 import Starling._
 import maker.Maker._
 
+
+// runs the 'bookmark' tests
 // nb: these are not really tests ATM, just a main method that does some testing...
 println("starling Bookmark tests...")
 
-debug 
+val br = for {
+  c <- starling.clean
+  u <- starling.update
+  b <- launcher.runMain("starling.bookmarkedtests.BookmarkTests")(commonLaunchArgs : _*)()
+} yield b
 
-val r = launcher.runMain("starling.bookmarkedtests.BookmarkTests")(commonLaunchArgs : _*)()
-
-println("Bookmark tests  completed")
+println("Bookmark tests have completed")
 
 handleExit(r)
 
