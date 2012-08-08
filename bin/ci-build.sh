1e595bf87e6557dc153d621aedd7c88db9493f57
Alex McGuire <alex.mcguire@trafigura.com>
2012-08-08 10:10:03 +0100

    git ignore maker test output
    Added maker settings file with traf specific stuff
    
    Copy latest maker script
    
    Make ci always recompile project files, and always boot strap maker.jar
    
    CI must now update, but NOT bootstrap maker
    
    Commiting debug line
    
    Commiting debug line
    
    More debug
    
    More debug - 2
    
    More debug - 3
    
    More debug - 4
    
    More debug - 5
    
    More debug - 6
    
    More debug - 8
    
    More debug - 9
    
    More debug - 10
    
    More debug - 11
    
    Remove ntty parameter from call to maker

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -37,7 +37,10 @@ fi
 
 echo "project file = $MAKER_PROJECT_FILE"
 
-./maker/dist/bin/maker.sh -ntty -c "maker/project/" -p $MAKER_PROJECT_FILE -args $ARGS | tee ci-build.log ; test ${PIPESTATUS[0]} -eq 0 || exit -1
+echo "LAUNCHING MAKER"
+export MAKER_DEBUG=true
+echo ./maker/dist/bin/maker.sh -ntty -c "maker/project/" -y -d -p $MAKER_PROJECT_FILE -args $ARGS 
+./maker/dist/bin/maker.sh -c "maker/project/" -y -d -p $MAKER_PROJECT_FILE -args $ARGS | tee ci-build.log ; test ${PIPESTATUS[0]} -eq 0 || exit -1
 
 if [ "$2" == "starling" ] && [ "$1" != "" ]; then
   echo "creating and publishing binary deploy artefacts..."
