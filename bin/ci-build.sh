a9cc726e521d83a34d073b45c966c69739be2b23
Thomas Rynne <thomas.rynne@trafigura.com>
2012-10-18 10:01:34 +0100

    Displaying jenkings build numbers in the git page for both git and binary releases

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -4,6 +4,7 @@ echo "Running ci-build.sh"
 echo "------------------------------------------------------"
 echo "JAVA_HOME: $JAVA_HOME"
 echo "SCALA_HOME: $SCALA_HOME"
+GIT_COMMIT=`git log -1 | head -n 1 | sed s/commit\ //`
 java -version
 $SCALA_HOME/bin/scala -version
 echo
@@ -15,6 +16,7 @@ echo "CPUs"
 grep processor /proc/cpuinfo
 echo "Memory"
 free -m
+echo "git commit: $GIT_COMMIT"
 echo "PATH: $PATH"
 echo "MAVEN_HOME: $MAVEN_HOME"
 echo "MAVEN_OPTS: $MAVEN_OPTS"
@@ -22,12 +24,14 @@ echo "------------------------------------------------------"
 echo
 echo "##teamcity[progressStart 'compile']"
 echo "Script args: $*"
-echo "version.number          : $1"
+echo "build.number            : $1"
 echo "build.type              : $2"
 echo "publishing.resolver     : $3"
+echo "git.commit              : $GIT_COMMIT"
+echo "jenkins.job             : $JOB_NAME"
 echo "maker project file name : $4"
 
-ARGS="-Dversion.number=$1 -Dbuild.type=$2 -Dpublishing.resolver=$3"
+ARGS="-Dbuild.number=$1 -Dbuild.type=$2 -Dpublishing.resolver=$3 -Dgit.commit=$GIT_COMMIT -Djenkins.job=$JOB_NAME"
 echo "maker jvm args : $ARGS"
 
 MAKER_PROJECT_FILE=$4
@@ -41,11 +45,11 @@ fi
 echo "project file = $MAKER_PROJECT_FILE"
 
 export MAKER_DEBUG=true
-echo ./maker/dist/bin/maker.sh -ntty -c "maker/project/" -y -d -p $MAKER_PROJECT_FILE -args $ARGS 
+echo ./maker/dist/bin/maker.sh -ntty -c "maker/project/" -y -d -p $MAKER_PROJECT_FILE -args $ARGS
 ./maker/dist/bin/maker.sh -c "maker/project/" -y -d -p $MAKER_PROJECT_FILE -args $ARGS | tee ci-build.log ; test ${PIPESTATUS[0]} -eq 0 || exit -1
 
 
-if [ "$2" == "starling" ] && [ "$1" != "" ]; then
+if [ "$2" == "starling" ] && ["$3" == "starling-release"]; then
   echo "creating and publishing binary deploy artefacts..."
   ./bin/create_artifact.sh $1 $3 || exit 3
 fi
