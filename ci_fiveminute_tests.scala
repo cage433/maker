fe572c16afafccfa99f01c91f1955d46ec1b68b9
Thomas Rynne <thomas.rynne@trafigura.com>
2012-11-13 16:36:26 +0000

    Test: an integration test which checks that the lim import still works (just oil at the moment)

---
+++ ci_fiveminute_tests.scala
@@ -0,0 +1,19 @@
+import Common._
+import Utils._
+import Starling._
+import maker.Maker._
+
+
+println("starling Five Minute tests...")
+
+val br = for {
+  c <- starling.clean
+  u <- starling.update
+  t <- launcher.testCompile
+  b <- launcher.runMain("starling.slowtests.FiveMinuteTests")(commonLaunchArgs : _*)()
+} yield b
+
+println("Five minute tests have completed")
+
+handleExit(br)
+
