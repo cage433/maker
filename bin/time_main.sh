8a24056469425dcf31e2e229ff6cea0a25c979f9
Thomas Rynne <thomas.rynne@trafigura.com>
2012-12-27 11:45:01 +0000

    Adds ability to run main can capture timings for jenkins

---
+++ bin/time_main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Runs a java main supplied as the only argument and records the cpu & gc time
+
+MAIN_CLASS=$1
+bin/maker -y -e "update;clean;testCompile;writeStarlingClasspathWithTests"
+#bin/maker -e "testCompile;writeStarlingClasspathWithTests"
+COMPILE_WORKED=$?
+
+if [ $COMPILE_WORKED -eq 0 ]; then
+  . bin/deploy-classpath.sh
+  #Note if you use 'time' instead of '/usr/bin/time' the commmand line options don't work. No idea why
+  /usr/bin/time --format "%S\n%U\n" --output=cputimes.txt \
+   java \
+   -server \
+   -XX:MaxPermSize=512m \
+   -Xms2000m \
+   -Xmx4000m \
+   -XX:-UseConcMarkSweepGC \
+   -verbose:gc \
+   -XX:+PrintGCTimeStamps \
+   -XX:+PrintGCDetails \
+   -Dsun.awt.disablegrab=true \
+   -Dlogback.configurationFile=./logback.xml \
+   starling.utils.RunMainWithStats $MAIN_CLASS
+
+   MAIN_WORKED=$?
+   if [ $COMPILE_WORKED -eq 1 ]; then
+     echo "failed"
+   fi
+
+   CPU_MILLIS=`cat cputimes.txt | awk '{s+=$1} END {print s * 1000}'`
+   #RunMainWithStats creates the CPU placeholder which is replaced here
+   cat times-input.csv | sed s/CPU_MILLIS/$CPU_MILLIS/ > times.csv
+
+   echo "Produced the following times:"
+   cat times.csv
+
+   exit $MAIN_WORKED
+ else
+   echo "compile failed"
+ fi
+
+
