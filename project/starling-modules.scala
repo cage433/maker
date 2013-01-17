2d432e9f82a2cf09762e529a1458c30bbf490a00
stacy.curl <stacy.curl@trafigura.com>
2013-01-17 16:27:50 +0000

    S-25426: Enable starling to use either Rabbit or AQ events
    We had many classes or pages that were called RabbitSomethingOrOther than weren't actually tied to rabbit, these are now called TitanSomethingOrOther and are not in a rabbit package or module
    
    Added AQTitanEventServices (TitanEventServices is the new name for RabbitEventServices, RabbitTitanEventServices is the new name for DefaultRabbitEventServices),
      however this ignores all DemultiplexerClients, this is because I can't find an AQ EventListener
    
    Deleted some unused rabbit code (ConnectionPool, ChannelPool)
    Replaced the RabbitEnabled property with TitanEventType, use TitanEventType=Rabbit in place of RabbitEnabled=true & TitanEventType=Disable in place of false
    
    Experimented a bit with compositional patching, see RenameTable

--- project/starling-modules.scala
+++ project/starling-modules.scala
@@ -75,16 +75,16 @@ object Starling {
   lazy val curves = project("curves", List(maths, guiapi), List(utils, quantity, daterange))
   lazy val instrument = project("instrument",  List(curves), List(utils, quantity, curves, daterange, maths))
   lazy val reportsFacility = project("reports.facility",  guiapi)
-  lazy val rabbitEventViewerApi = project("rabbit.event.viewer.api",  guiapi)
+  lazy val eventViewerApi = project("event.viewer.api",  guiapi)
   lazy val tradeFacility = project("trade.facility",  guiapi)
-  lazy val gui = project("gui", fc2Facility, tradeFacility, reportsFacility, browser, rabbitEventViewerApi, singleClasspathManager)
+  lazy val gui = project("gui", fc2Facility, tradeFacility, reportsFacility, browser, eventViewerApi, singleClasspathManager)
   lazy val starlingClient = project("starling.client", bouncyrmi)
   lazy val dbx = project("dbx", props)
   lazy val databases = project("databases", List(dbx, instrument), List(utils, curves, daterange))
   lazy val schemaevolution = project("schemaevolution", databases)
   lazy val titan = project("titan", List(reportsImpl), List(utils, instrument, quantity, curves, daterange, reportsImpl))
   lazy val services = project("services", List(loopyxl, schemaevolution, fc2Facility, reportsFacility), List(utils, quantity, curves, daterange, instrument))
-  lazy val rabbitEventViewerService = project("rabbit.event.viewer.service", rabbitEventViewerApi, titan)
+  lazy val eventViewerService = project("event.viewer.service", eventViewerApi, titan)
   lazy val tradeImpl = project("trade.impl", services, tradeFacility)
   lazy val pnlreconcile = project("pnlreconcile", List(services, tradeFacility), List(utils, curves, daterange))
   lazy val reportsImpl = project("reports.impl", List(pnlreconcile), List(utils, quantity, curves, daterange))
@@ -94,7 +94,7 @@ object Starling {
   lazy val webservice = project("webservice", List(titan), List(utils))
 
   // below are some utils for running starling from maker
-  lazy val startserver = project("startserver", oil, tradeImpl, starlingClient, webservice, rabbitEventViewerService, singleClasspathManager)
+  lazy val startserver = project("startserver", oil, tradeImpl, starlingClient, webservice, eventViewerService, singleClasspathManager)
   lazy val launcher = project("launcher", List(startserver, booter, gui), List(curves))
   lazy val starling = new TopLevelProject("starling", List(launcher), makerProps,
     List(
