91515bc3fb64a1492438094582e673ca1add6e0f
Alex.McGuire <alex.mcguire@trafigura.com>
2013-02-28 17:27:33 +0000

    New version of maker

--- dist/bin/maker.sh
+++ dist/bin/maker.sh
@@ -27,12 +27,17 @@ MAKER_PROJECT_ROOT_DIR=`pwd`
 
 set -e
 
+echo "Java version"
+echo `java -version`
 MAKER_OWN_LIB_DIR=$MAKER_OWN_ROOT_DIR/.maker/lib
-MAKER_IVY_SETTINGS_FILE=ivysettings.xml
-MAKER_COMPILED_PROJ_OUTPUT_DIR=$MAKER_OWN_ROOT_DIR/.maker/proj
+MAKER_OWN_SCALA_LIB_DIR=$MAKER_OWN_ROOT_DIR/.maker/scala-libs
+MAKER_OWN_IVY_SETTINGS_FILE=$MAKER_OWN_ROOT_DIR/ivysettings.xml
+MAKER_COMPILED_PROJ_INPUT_DIR=$MAKER_PROJECT_ROOT_DIR/.maker/src
+MAKER_COMPILED_PROJ_OUTPUT_DIR=$MAKER_PROJECT_ROOT_DIR/.maker/classes
 MAKER_OWN_SCALATEST_REPORTER_JAR=$MAKER_OWN_ROOT_DIR/maker-scalatest-reporter.jar
 MAKER_OWN_SCALATEST_REPORTER_SOURCE=$MAKER_OWN_ROOT_DIR/test-reporter/src/maker/scalatest/MakerTestReporter.scala
-MAKER_OWN_JAR=$MAKER_OWN_ROOT_DIR/maker.jar 
+MAKER_OWN_BOOTSTRAP_DIR=$MAKER_OWN_ROOT_DIR/bootstrap
+echo "Bootstrap " + $MAKER_OWN_BOOTSTRAP_DIR
 
 debug(){
   msg=$1
@@ -47,6 +52,14 @@ main() {
   check_setup_sane || exit -1
   write_ivy_files
 
+  if [ $MAKER_IVY_UPDATE ] || [ ! -e $MAKER_OWN_SCALA_LIB_DIR ];
+  then
+    echo "Updating scala libraries"
+    scala_ivy_update
+  else
+    echo "Omitting scala ivy update as $MAKER_OWN_SCALA_LIB_DIR exists"
+  fi
+
   if [ $MAKER_IVY_UPDATE ] || [ ! -e $MAKER_OWN_LIB_DIR ];
   then
     ivy_update
@@ -60,11 +73,11 @@ main() {
     ivy_update
     maker_ivy_binary_retrieve
   else
-    if [[ ! -z $MAKER_BOOTSTRAP ]] || [[ ! -e $MAKER_OWN_JAR ]] ;
+    if [[ ! -z $MAKER_BOOTSTRAP ]] || [[ ! -e $MAKER_OWN_ROOT_DIR/maker.jar ]] ;
     then
       bootstrap || exit -1
     else
-      echo "Omitting bootstrap as $MAKER_OWN_JAR exists"
+      echo "Omitting bootstrap as $MAKER_OWN_ROOT_DIR/maker.jar exists"
     fi
 
     if test $MAKER_OWN_SCALATEST_REPORTER_SOURCE -nt $MAKER_OWN_SCALATEST_REPORTER_JAR;
@@ -75,9 +88,9 @@ main() {
 
   if [ -z $MAKER_SKIP_LAUNCH ];
   then
-    JAVA_OPTS=" -Xmx$(($MAKER_HEAP_SPACE))m -XX:MaxPermSize=$(($MAKER_PERM_GEN_SPACE))m $JREBEL_OPTS $MAKER_DEBUG_PARAMETERS -XX:+HeapDumpOnOutOfMemoryError "$JAVA_OPTS" "
+    JAVA_OPTS=" -Xmx$(($MAKER_HEAP_SPACE))m -XX:MaxPermSize=$(($MAKER_PERM_GEN_SPACE))m $JREBEL_OPTS $MAKER_DEBUG_PARAMETERS -XX:+HeapDumpOnOutOfMemoryError -XX:+UseCodeCacheFlushing -XX:ReservedCodeCacheSize=256m -XX:+CMSClassUnloadingEnabled "$JAVA_OPTS" "
     # TODO - move scala jars from bootclasspath to classpath once permgen fix available
-    CLASSPATH="$(maker_internal_classpath):$(external_jars):$MAKER_OWN_ROOT_DIR/resources/"
+    CLASSPATH="$(maker_internal_classpath):$(external_jars):${scala_jars}:$MAKER_OWN_ROOT_DIR/resources/"
 #    echo "CLASSPATH = $CLASSPATH"
 #    echo "Args = $MAKER_ARGS"
 
@@ -96,6 +109,7 @@ main() {
 
       # are we already up to date?
       # look for any sources newer than all compiled sources, if so we need to recompile!
+      mkdir -p $MAKER_COMPILED_PROJ_OUTPUT_DIR 
       for class in `find $MAKER_COMPILED_PROJ_OUTPUT_DIR`; do
         if [ "$(find $MAKER_COMPILED_PROJ_INPUT_DIR -anewer $class)" != "" ]; then
           RECOMPILE_PROJECT=true
@@ -116,7 +130,8 @@ main() {
 
 	    # compile the maker project files in the -c specified input dir
 	    echo "compiling to $MAKER_COMPILED_PROJ_OUTPUT_DIR"
-        $SCALA_HOME/bin/scalac -classpath "$(external_jars):$CLASSPATH" -d $MAKER_COMPILED_PROJ_OUTPUT_DIR-TMP $MAKER_COMPILED_PROJ_INPUT_FILES | tee $MAKER_OWN_ROOT_DIR/proj-compile-output ; test ${PIPESTATUS[0]} -eq 0 || exit -1
+        debug "java -classpath $(external_jars):$CLASSPATH -Dscala.usejavacp=true scala.tools.nsc.Main -d $MAKER_COMPILED_PROJ_OUTPUT_DIR-TMP $MAKER_COMPILED_PROJ_INPUT_FILES"
+        java -classpath "$(external_jars):$CLASSPATH" -Dscala.usejavacp=true  scala.tools.nsc.Main -d $MAKER_COMPILED_PROJ_OUTPUT_DIR-TMP $MAKER_COMPILED_PROJ_INPUT_FILES | tee $MAKER_OWN_ROOT_DIR/proj-compile-output ; test ${PIPESTATUS[0]} -eq 0 || exit -1
 
         # if we got here then we're looking all ok to start using this compiled project
         mv $MAKER_COMPILED_PROJ_OUTPUT_DIR-TMP/ $MAKER_COMPILED_PROJ_OUTPUT_DIR/
@@ -127,7 +142,7 @@ main() {
     fi
 
     # launcher maker in the repl, with the compiled project definitions on the classpath and scripted project definition files interpreted using the -i option on scala repl
-    $JAVA_HOME/bin/java $JAVA_OPTS -Xbootclasspath/a:$(scala_jars) -classpath $CLASSPATH -Dmaker.home="$MAKER_OWN_ROOT_DIR" -Dlogback.configurationFile=logback.xml -Dmaker.process.hierarchy="repl" $RUNNING_EXEC_MODE -Dmaker.level="0" -Dscala.usejavacp=true $MAKER_ARGS scala.tools.nsc.MainGenericRunner -Yrepl-sync -nc -i $MAKER_PROJECT_FILE $CMDS | tee maker-session.log ; scala_exit_status=${PIPESTATUS[0]}
+    $JAVA_HOME/bin/java $JAVA_OPTS -classpath $CLASSPATH -Dsbt.log.format="false" -Dmaker.home="$MAKER_OWN_ROOT_DIR" -Dlogback.configurationFile=$MAKER_OWN_ROOT_DIR/logback.xml $RUNNING_EXEC_MODE -Dscala.usejavacp=true $MAKER_ARGS scala.tools.nsc.MainGenericRunner -Yrepl-sync -nc -i $MAKER_PROJECT_FILE $CMDS | tee maker-session.log ; scala_exit_status=${PIPESTATUS[0]}
   fi
 }
 
@@ -138,7 +153,7 @@ maker_internal_classpath(){
       cp="$cp:$MAKER_OWN_ROOT_DIR/$module/target-maker/classes:$MAKER_OWN_ROOT_DIR/$module/target-maker/test-classes/:$MAKER_OWN_SCALATEST_REPORTER_JAR"
     done
   else
-    cp="$MAKER_OWN_JAR:$MAKER_OWN_SCALATEST_REPORTER_JAR"
+    cp="$MAKER_OWN_ROOT_DIR/maker.jar:$MAKER_OWN_SCALATEST_REPORTER_JAR"
   fi
   for module in utils plugin maker; do
     cp="$cp:$MAKER_OWN_ROOT_DIR/$module/resources/"
@@ -148,11 +163,6 @@ maker_internal_classpath(){
 
 check_setup_sane(){
   debug "Checking sanity"
-  if [ -z $SCALA_HOME ];
-  then
-    echo "SCALA_HOME not defined"
-    exit -1
-  fi
 
   if [ -z $JAVA_HOME ];
   then
@@ -163,7 +173,7 @@ check_setup_sane(){
   fetch_ivy
   echo "done fetching ivy"
 
-  MAKER_IVY_JAR=${MAKER_IVY_JAR-${MAKER_OWN_ROOT_DIR}/libs/ivy-2.3.0-rc2.jar}
+  MAKER_IVY_JAR=${MAKER_IVY_JAR-${MAKER_OWN_BOOTSTRAP_DIR}/ivy-2.3.0-rc2.jar}
   if [ ! -e $MAKER_IVY_JAR ];
   then
     echo "Ivy jar not found"
@@ -210,14 +220,13 @@ run_command(){
 }
 
 external_jars() {
-  cp=`ls $MAKER_OWN_ROOT_DIR/.maker/lib/*.jar | xargs | sed 's/ /:/g'`
-  #cp=$cp:`ls $MAKER_OWN_ROOT_DIR/.maker/scala-lib/*.jar | xargs | sed 's/ /:/g'`
-  cp=$cp:`ls $MAKER_OWN_ROOT_DIR/libs/*.jar | xargs | sed 's/ /:/g'`
+  cp=`ls $MAKER_OWN_LIB_DIR/*.jar | xargs | sed 's/ /:/g'`
+  cp=$cp:`ls $MAKER_OWN_SCALA_LIB_DIR/*.jar | xargs | sed 's/ /:/g'`
   echo $cp
 }
 
 scala_jars(){
-  jars=`ls $SCALA_HOME/lib/*.jar | xargs | sed 's/ /:/g'`
+  jars=`ls $MAKER_OWN_SCALA_LIB_DIR/*.jar | xargs | sed 's/ /:/g'`
   echo $jars
 }
 
@@ -226,7 +235,9 @@ build_test_reporter_jar() {
     rm -f $MAKER_OWN_SCALATEST_REPORTER_JAR
     TEMP_OUTPUT_DIR=`mktemp -d maker-tmp-XXXXXXXXXX`
 
-    $SCALA_HOME/bin/scalac -classpath $(external_jars) -d $TEMP_OUTPUT_DIR $MAKER_OWN_SCALATEST_REPORTER_SOURCE 2>&1 | tee $MAKER_OWN_ROOT_DIR/vim-compile-output ; test ${PIPESTATUS[0]} -eq 0 || exit 44 
+    debug "java -classpath $(external_jars) -Dscala.usejavacp=true scala.tools.nsc.Main -d $TEMP_OUTPUT_DIR $MAKER_OWN_SCALATEST_REPORTER_SOURCE"
+
+    java -classpath $(external_jars) -Dscala.usejavacp=true scala.tools.nsc.Main -d $TEMP_OUTPUT_DIR $MAKER_OWN_SCALATEST_REPORTER_SOURCE 2>&1 | tee $MAKER_OWN_ROOT_DIR/vim-compile-output ; test ${PIPESTATUS[0]} -eq 0 || exit 44 
 
     run_command "$JAVA_HOME/bin/jar cf $MAKER_OWN_SCALATEST_REPORTER_JAR -C $TEMP_OUTPUT_DIR . " || exit -1
     rm -rf $TEMP_OUTPUT_DIR
@@ -237,6 +248,7 @@ bootstrap() {
   pushd $MAKER_OWN_ROOT_DIR  # Shouldn't be necessary to change dir, but get weird compilation errors otherwise
   TEMP_OUTPUT_DIR=`mktemp -d maker-tmp-XXXXXXXXXX`
   MAKER_OWN_RESOURCES_DIR=$MAKER_OWN_ROOT_DIR/utils/resources
+  MAKER_OWN_JAR=$MAKER_OWN_ROOT_DIR/maker.jar
 
   rm -f $MAKER_OWN_JAR
 
@@ -245,10 +257,11 @@ bootstrap() {
   done
 
   echo "Building maker.jar"
-  $SCALA_HOME/bin/scalac -classpath $(external_jars) -d $TEMP_OUTPUT_DIR $SRC_FILES 2>&1 | tee $MAKER_OWN_ROOT_DIR/vim-compile-output ; test ${PIPESTATUS[0]} -eq 0 || exit -1
+  debug "java -classpath $(external_jars) -Dscala.usejavacp=true scala.tools.nsc.Main  -d $TEMP_OUTPUT_DIR $SRC_FILES "
+  java -classpath $(external_jars) -Dscala.usejavacp=true scala.tools.nsc.Main  -d $TEMP_OUTPUT_DIR $SRC_FILES 2>&1 | tee $MAKER_OWN_ROOT_DIR/vim-compile-output ; test ${PIPESTATUS[0]} -eq 0 || exit -1
   run_command "$JAVA_HOME/bin/jar cf $MAKER_OWN_JAR -C $TEMP_OUTPUT_DIR . -C $MAKER_OWN_RESOURCES_DIR ." || exit -1
 
-  if [ ! -e $MAKER_OWN_JAR ];
+  if [ ! -e $MAKER_OWN_ROOT_DIR/maker.jar ];
   then
 	  echo "Maker jar failed to be created"
 	  exit -1
@@ -269,14 +282,13 @@ process_options() {
       -r | --revision ) MAKER_BINARY_VERSION=$2; shift 2;;
       -p | -i | --project-file ) MAKER_PROJECT_FILE=$2; shift 2;;
       -c | --project-input-dir ) MAKER_COMPILED_PROJ_INPUT_DIR=$2; shift 2;;
-      -d | --clean-project-class-files) rm -rf $MAKER_COMPILED_PROJ_OUTPUT_DIR; shift 1;;
+      -d | --clean-project-class-files) rm -rf $MAKER_COMPILED_PROJ_OUTPUT_DIR; RECOMPILE_PROJECT=true; shift 1;;
       -e | --exec-cmd ) MAKER_CMD=$2; shift 2;;
       -j | --use-jrebel ) set_jrebel_options; shift;;
       -m | --mem-heap-space ) MAKER_HEAP_SPACE=$2; shift 2;;
       -y | --do-ivy-update ) MAKER_IVY_UPDATE=true; shift;;
       -b | --boostrap ) MAKER_BOOTSTRAP=true; shift;;
       -x | --allow-remote-debugging ) MAKER_DEBUG_PARAMETERS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"; shift;;
-      -w | --maker-jar ) MAKER_OWN_JAR=$2; shift 2;;
       -z | --developer-mode ) MAKER_DEVELOPER_MODE=true; shift;;
       -nr | --no-repl ) MAKER_SKIP_LAUNCH=true; shift 1;;
       -ntty | --no-tty-restore ) echo; echo "DEPRECATED OPTION '-ntty', THIS CAN BE REMOVED"; echo; shift 1;;
@@ -287,7 +299,7 @@ process_options() {
       --ivy-non-proxy-hosts ) MAKER_IVY_NON_PROXY_HOSTS=$2; shift 2;; 
       --ivy-jar ) MAKER_IVY_JAR=$2; shift 2;;
       --ivy-url ) MAKER_IVY_URL=$2; shift 2;;
-      --ivy-settings-file ) MAKER_IVY_SETTINGS_FILE=$2; shift 2;;
+      --ivy-settings-file ) MAKER_OWN_IVY_SETTINGS_FILE=$2; shift 2;;
       -- ) shift; break;;
       *  ) break;;
     esac
@@ -366,7 +378,7 @@ ivy_command(){
     command="$command -Dhttp.nonProxyHosts=$MAKER_IVY_NON_PROXY_HOSTS"
   fi
   command="$command -Dmaker_binary_version=$MAKER_BINARY_VERSION -jar $MAKER_IVY_JAR -ivy $ivy_file"
-  command="$command -settings $MAKER_OWN_ROOT_DIR/$MAKER_IVY_SETTINGS_FILE "
+  command="$command -settings $MAKER_OWN_IVY_SETTINGS_FILE "
   command="$command -retrieve $lib_dir/[artifact]-[revision](-[classifier]).[ext] "
   echo $command
 }
@@ -380,6 +392,12 @@ ivy_update() {
   run_command "$(ivy_command $MAKER_IVY_FILE $MAKER_OWN_LIB_DIR) -types source"
 }
 
+scala_ivy_update() {
+  debug "Updating scala libraries (using Ivy)"
+  MAKER_SCALA_IVY_FILE="$MAKER_OWN_ROOT_DIR/scala/ivy.xml"
+  run_command "$(ivy_command $MAKER_SCALA_IVY_FILE $MAKER_OWN_SCALA_LIB_DIR) -types jar -sync"
+  run_command "$(ivy_command $MAKER_SCALA_IVY_FILE $MAKER_OWN_SCALA_LIB_DIR) -types source"
+}
 maker_ivy_binary_retrieve() {
   debug "Fetching maker binary dependencies (using Ivy) for version ${MAKER_BINARY_VERSION}"
   MAKER_IVY_FILE="$MAKER_OWN_ROOT_DIR/maker-ivy.xml"
@@ -393,7 +411,7 @@ fetch_ivy() {
   if [ -z $MAKER_IVY_URL ]; then
     MAKER_IVY_URL="http://repo1.maven.org/maven2/org/apache/ivy/ivy/2.3.0-rc2/ivy-2.3.0-rc2.jar"
   fi
-  if [ ! -f ${MAKER_OWN_ROOT_DIR}/libs/ivy-2.3.0-rc2.jar ]; then
+  if [ ! -f ${MAKER_OWN_BOOTSTRAP_DIR}/ivy-2.3.0-rc2.jar ]; then
     if [ ! -z $MAKER_IVY_PROXY_HOST ]; then
       CURL_PROXY_ARGS="-x http://$MAKER_IVY_PROXY_HOST:$MAKER_IVY_PROXY_PORT"
     fi
@@ -444,6 +462,7 @@ function write_ivy_files() {
   debug "Writing ivy files"
 
   mkdir -p ${MAKER_OWN_ROOT_DIR}/utils/
+  mkdir -p ${MAKER_OWN_ROOT_DIR}/scala/
   mkdir -p ${MAKER_OWN_ROOT_DIR}/test-reporter/
 
   cat > ${MAKER_OWN_ROOT_DIR}/utils/ivy.xml<<'IVY_FILE'
@@ -464,21 +483,19 @@ function write_ivy_files() {
   </publications>
 
   <dependencies defaultconfmapping="*->default">
-    <dependency org="org.scala-lang" name="scala-compiler" rev="${scala_version}" />
-    <dependency org="org.scala-lang" name="scala-library" rev="${scala_version}" />
     <dependency org="commons-io" name="commons-io" rev="2.1"/>
     <dependency org="commons-codec" name="commons-codec" rev="1.6"/>
     <dependency org="org.apache.commons" name="commons-lang3" rev="3.1"/>
-    <dependency org="org.scala-tools.testing" name="scalacheck_2.9.1" rev="1.9"/>
-    <dependency org="org.scalatest" name="scalatest_2.9.1" rev="${scalatest_version}"/>
-    <dependency org="org.scalaz" name="scalaz-core_2.9.1" rev="6.0.4"/>
+    <dependency org="org.scala-tools.testing" name="scalacheck_${scala_version}" rev="1.9"/>
+    <dependency org="org.scalatest" name="scalatest_${scala_version}" rev="${scalatest_version}"/>
+    <dependency org="org.scalaz" name="scalaz-core_${scala_version}" rev="6.0.4"/>
     <dependency org="ch.qos.logback" name="logback-classic" rev="1.0.6"/> 
     <dependency org="ch.qos.logback" name="logback-core" rev="1.0.6"/> 
     <dependency org="org.slf4j" name="slf4j-api" rev="1.6.1"/>
     <dependency org="org.apache.ant" name="ant" rev="1.8.2"/>
     <dependency org="io.netty" name="netty" rev="3.4.2.Final"/>
     <dependency org="com.google.protobuf" name="protobuf-java" rev="2.4.1"/>
-    <dependency org="net.debasishg" name="sjson_2.9.1" rev="0.17"/>
+    <dependency org="net.debasishg" name="sjson_${scala_version}" rev="0.17"/>
     <dependency org="org.eclipse.jetty" name="jetty-server" rev="${jetty_version}" />
     <dependency org="org.eclipse.jetty" name="jetty-webapp" rev="${jetty_version}" />
     <dependency org="org.eclipse.jetty" name="jetty-util" rev="${jetty_version}" />
@@ -488,7 +505,7 @@ function write_ivy_files() {
     <dependency org="org.eclipse.jetty" name="jetty-io" rev="${jetty_version}" />
     <dependency org="org.eclipse.jetty" name="jetty-xml" rev="${jetty_version}" />
     <dependency org="org.eclipse.jetty" name="jetty-continuation" rev="${jetty_version}" />
-    <dependency org="org.eclipse.jetty" name="jetty-jsp" rev="${jetty_version}" />
+    <!-- <dependency org="org.eclipse.jetty" name="jetty-jsp" rev="${jetty_version}" /> -->
     <dependency org="redis.clients" name="jedis" rev="2.0.0" />
     <dependency org="commons-pool" name="commons-pool" rev="1.5.5" />
     <dependency org="org.mortbay.jetty" name="jsp-2.1-glassfish" rev="2.1.v20100127" />
@@ -496,6 +513,11 @@ function write_ivy_files() {
     <dependency org="org.apache.tomcat" name="jsp-api" rev="6.0.20" />
     <dependency org="org.mockito" name="mockito-all" rev="1.8.2" />
     <dependency org="org.apache.ivy" name="ivy" rev="2.3.0-rc2" />
+    <dependency org="com.typesafe.zinc" name="zinc" rev="0.2.1"/>
+    <dependency org="com.typesafe.sbt" name="incremental-compiler" rev="${sbt_version}"/>
+    <dependency org="org.scala-lang" name="jline" rev="${scala_version}" />
+    <dependency org="org.scala-lang" name="scala-compiler" rev="${scala_version}" />
+    <dependency org="org.scala-lang" name="scala-library" rev="${scala_version}" />
   </dependencies>
 </ivy-module>
 IVY_FILE
@@ -518,7 +540,7 @@ cat > ${MAKER_OWN_ROOT_DIR}/test-reporter/ivy.xml<<'TEST_REPORTER_IVY_FILE'
   </publications>
 
   <dependencies defaultconfmapping="*->default,sources">
-    <dependency org="org.scalatest" name="scalatest_2.9.1" rev="${scalatest_version}"/>
+    <dependency org="org.scalatest" name="scalatest_${scala_version}" rev="${scalatest_version}"/>
   </dependencies>
 </ivy-module>
 TEST_REPORTER_IVY_FILE
@@ -539,13 +561,38 @@ cat > ${MAKER_OWN_ROOT_DIR}/maker-ivy.xml <<'MAKER_IVY_FILE'
 </ivy-module>
 MAKER_IVY_FILE
 
-if [ ! -e $MAKER_IVY_SETTINGS_FILE ]; then
-  cat > $MAKER_IVY_SETTINGS_FILE <<'IVY_SETTINGS'
+cat > ${MAKER_OWN_ROOT_DIR}/scala/ivy.xml<<'SCALA_IVY_FILE'
+<!-- Auto-generated from Maker script -->
+<ivy-module version="1.0" xmlns:e="http://ant.apache.org/ivy/extra">
+  <info organisation="${group_id}" module="utils" revision="${maker.module.version}" />
+  <configurations>
+    <conf name="default" transitive="false"/>
+    <conf name="compile" transitive="false"/>
+    <conf name="test" transitive="false"/>
+  </configurations>
+
+  <publications>
+    <artifact name="utils" type="pom"/>
+    <artifact name="utils" type="jar" ext="jar" conf="default" />
+    <artifact name="utils-sources" type="jar" ext="jar" conf="default" />
+    <artifact name="utils-docs" type="jar" ext="jar" conf="default" />
+  </publications>
+
+  <dependencies defaultconfmapping="*->default">
+    <dependency org="org.scala-lang" name="scala-compiler" rev="${scala_version}" />
+    <dependency org="org.scala-lang" name="scala-library" rev="${scala_version}" />
+    <dependency org="com.typesafe.sbt" name="sbt-interface" rev="${sbt_version}"/>
+  </dependencies>
+</ivy-module>
+SCALA_IVY_FILE
+
+cat > $MAKER_OWN_IVY_SETTINGS_FILE <<'IVY_SETTINGS'
 <!-- Auto-generated from Maker script -->
 <ivysettings>
   <property name="group_id" value="com.google.code.maker" />
   <property name="maker.module.version" value="0.01" />
   <property name="scala_version" value="2.9.1" />
+  <property name="sbt_version" value="0.12.1" />
   <property name="scalatest_version" value="1.8" />
   <property name="ivy.local.default.root" value="${ivy.default.ivy.user.dir}/maker-local" override="false"/>
   <property name="jetty_version" value="7.6.3.v20120416" />
@@ -578,7 +625,6 @@ if [ ! -e $MAKER_IVY_SETTINGS_FILE ]; then
   </resolvers>
 </ivysettings>
 IVY_SETTINGS
-fi
 }
 
 scala_exit_status=127
