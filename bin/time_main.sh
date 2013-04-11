9cc4ff2f6e5870e592cd7426bd1947a3c5277430
Alex.McGuire <alex.mcguire@trafigura.com>
2013-04-11 19:07:20 +0100

    Rebuild maker for 5 minute tests

--- bin/time_main.sh
+++ bin/time_main.sh
@@ -4,7 +4,7 @@
 
 MAIN_CLASS=$1
 shift
-bin/maker -d -y -e "update;clean;testCompile;writeStarlingClasspathWithTests"
+bin/maker -b -d -y -e "update;clean;testCompile;writeStarlingClasspathWithTests"
 #bin/maker -e "testCompile;writeStarlingClasspathWithTests"
 COMPILE_WORKED=$?
 
