0aeb882537d6eccbe5363b11c640d4118f025794
Louis Botterill <louis.botterill@trafigura.com>
2013-05-16 14:45:45 +0100

    fix idea files, speed up maker starling load, fix typo

--- Starling.scala
+++ Starling.scala
@@ -1,15 +1,7 @@
-println("\n ** Loading Starling build (script)  ...\n")
-
+println("\n ** Loading Starling build...\n")
 import java.io.File; import org.apache.commons.io.FileUtils._
-import maker.project._;
-import maker.utils.FileUtils._;
-import maker.task.BuildResult;
-//import maker.Maker._;
-import maker.MakerProps._
-import Common._;
-import Utils._;
-import Starling._;
-import Starling.starling._;
+import maker.project._; import maker.utils.FileUtils._; import maker.task.BuildResult; import maker.MakerProps._
+import Common._; import Utils._; import Starling._; import Starling.starling._;
 System.setProperty("scala.usejavacp", "false")
 
 println("\nStarling build loaded\n\nNote: for convenience the 'starling' project is in the root scope, clean, test etc will act on that unless a project is specified (e.g. utils.clean...)\n")
