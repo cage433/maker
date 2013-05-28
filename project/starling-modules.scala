abee49be69d2ca1595a559b9ba53ee8be5fae7d4
stacy.curl <stacy.curl@trafigura.com>
2013-05-28 16:45:44 +0100

    Remove the webservice => titan dependency, because oil uses webservice
    Quite a few consequences:
      EDMFormats is split into StarlingFormats, TitanFormats & OilFormats
      JsonSerializerMessageBodyWriter, JsonDeserializerMessageBodyReader, XmlSerializerMessageBodyWriter are now given a Formats
        and there are Titan* & Oil* derived classes that do this (because Resteasy cannot be given instances, only class names)

--- project/starling-modules.scala
+++ project/starling-modules.scala
@@ -83,66 +83,67 @@ object Starling {
 
   // projects ordered by layer, then alphabetically, each layer depends only on layers above
   // the dependencies of each project are also ordered by their position in this list.
-  // OCD yes but helps to visualise the project. There is a school of thought that too many layers is bad, we have 18!
+  // OCD yes but helps to visualise the project. There is a school of thought that too many layers is bad, we have 19!
+
+  // Layer 1
   lazy val booter = project("booter")
   lazy val loopyxlJava = project("loopyxl-java")
   lazy val manager = project("manager")
-
+  // Layer 2
   lazy val browserService = project("browser.service", manager)
   lazy val utils = project("utils", manager)
-
+  // Layer 3
   lazy val browser = project("browser", browserService)
   lazy val props = project("props", utils)
   lazy val starlingDTOApi = project("starling.dto.api", utils)
-
+  // Layer 4
   lazy val daterange = project("daterange", starlingDTOApi)
   lazy val quantity = project("quantity", List(starlingDTOApi), List(utils))
   lazy val singleClasspathManager = project("singleclasspathmanager", utils)
-
+  // Layer 5
   lazy val auth = project("auth", daterange)
   lazy val dbx = project("dbx", props, daterange)
   lazy val maths = project("maths", List(daterange, quantity), List(daterange, quantity))
   lazy val pivot = project("pivot", List(quantity), List(utils))
-
+  // Layer 6
   lazy val bouncyrmi = project("bouncyrmi", List(auth), List(utils))
   lazy val loopyxl = project("loopyxl", loopyxlJava, auth)
   lazy val pivotUtils = project("pivot.utils", daterange, pivot)
   lazy val schemaevolution = project("schemaevolution", dbx) // Please do not change this dependency without asking
-
+  // Layer 7
   lazy val guiapi = project("gui.api", browserService, bouncyrmi, pivotUtils)
   lazy val starlingClient = project("starling.client", bouncyrmi)
-
+  // Layer 8
   lazy val curves = project("curves", List(maths, guiapi), List(daterange, quantity))
   lazy val eventViewerApi = project("event.viewer.api", guiapi)
   lazy val fc2Facility = project("fc2.facility",  guiapi)
   lazy val reportsFacility = project("reports.facility", guiapi)
   lazy val tradeFacility = project("trade.facility", guiapi)
-
+  // Layer 9
   lazy val gui = project("gui", browser, singleClasspathManager, eventViewerApi, fc2Facility, reportsFacility, tradeFacility)
   lazy val instrument = project("instrument", List(curves), List(maths, curves))
-
+  // Layer 10
   lazy val databases = project("databases", List(dbx, instrument), List(curves))
-
+  // Layer 11
   lazy val services = project("services", List(loopyxl, fc2Facility, reportsFacility, databases), List(instrument))
-
+  // Layer 12
   lazy val tradeImpl = project("trade.impl", tradeFacility, services)
   lazy val pnlreconcile = project("pnlreconcile", List(tradeFacility, services), List(schemaevolution, curves))
-
+  lazy val webservice = project("webservice", List(services), List(utils))
+  // Layer 13
+  lazy val eventstoreServer = project("eventstore-server", List(webservice), List(utils))
   lazy val reportsImpl = project("reports.impl", List(schemaevolution, pnlreconcile), List(databases))
-
-  lazy val oil = project("oil", reportsImpl)
-  lazy val titan = project("titan", List(starlingClient, reportsImpl), List(instrument, reportsImpl))
-
+  // Layer 14
+  lazy val oil = project("oil", webservice, reportsImpl)
+  lazy val titan = project("titan", List(starlingClient, webservice, reportsImpl), List(instrument, webservice))
+  // Layer 15
   lazy val eventViewerService = project("event.viewer.service", eventViewerApi, titan)
-  lazy val webservice = project("webservice", List(titan), List(utils))
-
-  lazy val startserver = project("startserver", singleClasspathManager, tradeImpl, oil, eventViewerService, webservice)
-
+  // Layer 16
+  lazy val startserver = project("startserver", singleClasspathManager, tradeImpl, oil, webservice, eventViewerService)
+  // Layer 17
   lazy val launcher = project("launcher", List(booter, gui, startserver), List(curves))
-
-  lazy val eventstoreServer = project("eventstore-server", List(databases,webservice), List(utils))
-
-  lazy val starling = new TopLevelProject("starling", List(launcher,eventstoreServer), makerProps,
+  // Layer 18
+  lazy val starling = new TopLevelProject("starling", List(launcher, eventstoreServer), makerProps,
     List(
       "logs",
       "osgi-gui-cache",
