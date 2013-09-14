27a8c6e818f5b58688c33e68959e92cfb58e87ed
Alex.McGuire <alex.mcguire@trafigura.com>
2013-09-14 21:25:05 +0100

    Added self writing project test

--- maker/tests/maker/project/ProjectTests.scala
+++ maker/tests/maker/project/ProjectTests.scala
@@ -5,38 +5,52 @@ import maker.utils.FileUtils._
 import maker.utils.os.Command
 import maker.MakerProps
 
-
-class TestModuleTests extends FunSuite{
-  test("Can build test module from itself"){
-    withTempDir{
-      dir => 
-        val proj = new TestModule(dir, "TestTestModule")
-        proj.writeSrc(
+class ProjectTests extends FunSuite {
+  test("Project can write its own definition file"){
+    withTestDir{
+      dir =>{
+        val a = new TestModule(
+          mkdir(file(dir, "a")),
+          "a"
+        )
+        a.writeSrc(
           "foo/Foo.scala",
           """
           package foo
+
           case class Foo(a : Int)
           """
         )
-        proj.writeTest(
-          "foo/FooScalaTest.scala",
+
+        val b = new TestModule(
+          mkdir(file(dir, "b")),
+          "b",
+          List(a)
+        )
+        b.writeTest(
+          "foo/FooTest.scala",
           """
           package foo
           import org.scalatest.FunSuite
-          class FooScalaTest extends FunSuite{
-            test("Test Foo"){
-              val foo = Foo(3)
-              assert(foo.a == 3)
+          class FooTest extends FunSuite{
+            test("test foo"){
+              assert(Foo(3).a === 3)
             }
           }
           """
+        )
+
+        val proj = Project(
+          "TestProject",
+          dir,
+          List(a, b)
         )
         proj.writeMakerProjectDefinitionFile
         val makerDotSh = file("bin/maker.sh").absPath
         file("resource-resolvers").copyTo(dir)
         file("resource-versions").copyTo(dir)
         writeToFile(
-          proj.resourcesFile,
+          a.resourcesFile,
           "org.scalatest scalatest_{scala_version} {scalatest_version}"
         )
 
@@ -44,10 +58,11 @@ class TestModuleTests extends FunSuite{
           MakerProps(file(dir, "props.conf")),
           makerDotSh,
           "-z",      // Developer mode prevents maker bootstrapping 
-          "-e", "TestTestModule.test"
+          "-e", "TestProject.test"
         ).withWorkingDirectory(dir)
         val result = cmd.exec
         assert(result === 0, "Expected project test to succeeed")
+      }
     }
   }
 }
