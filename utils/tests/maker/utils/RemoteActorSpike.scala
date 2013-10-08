ae5323603fd28eee855c69844ad467c94d47e78c
Alex.McGuire <alex.mcguire@trafigura.com>
2013-10-08 17:23:58 +0100

    Remove unused dependencies

---
+++ utils/tests/maker/utils/RemoteActorSpike.scala
@@ -0,0 +1,7 @@
+package maker.utils
+
+object RemoteActorSpike extends App{
+  class HelloWorld extends Actor{
+  }
+  println("hi")
+}
---
+++ utils/tests/maker/utils/RemoteActorTests.scala
@@ -0,0 +1,8 @@
+package maker.utils
+
+import org.scalatest.FunSuite
+
+class RemoteActorTests extends FunSuite {
+  test("Remote actor call"){
+  }
+}
