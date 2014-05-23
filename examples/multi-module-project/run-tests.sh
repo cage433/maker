34ea2cafd12f6058918d1cba51704bc305bd4403
Alex.McGuire <alex.mcguire@trafigura.com>
2014-05-23 14:42:24 +0100

    Document test script

--- examples/multi-module-project/run-tests.sh
+++ examples/multi-module-project/run-tests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Hack to ensure that maker unit tests that run the tests on this example
+# project are working correctly. This script is executed from the maker unit test
+# ExampleProjectTests.scala
+
+# Create a dummy file
 touch marker-file
+
+# Run this example project's tests - one of them should delete the dummy file
 ../../bin/maker.sh -e 'project.test'
 
+# Return to the maker unit test which invoked this script - ExampleProjectTests.scala
 if [ -e "marker-file" ]; then
   exit -1
 else
