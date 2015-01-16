52d97a5b14bbe1c38cc77ff14e9bd459256f9244
Alex.McGuire <alex.mcguire@trafigura.com>
2015-01-16 14:06:27 +0000

    Refactor publish task - minor

--- maker/src/maker/task/tasks/PublishTask.scala
+++ maker/src/maker/task/tasks/PublishTask.scala
@@ -10,6 +10,7 @@ import maker.task._
 import maker.utils.Stopwatch
 import maker.MakerProps
 import maker.utils.maven.IvyLock
+import scala.collection.JavaConversions._
 
 
 case class PublishTask(baseProject : BaseProject, resolverName : String, version : String) extends Task {
@@ -30,25 +31,29 @@ case class PublishTask(baseProject : BaseProject, resolverName : String, version
     val ivyFile = baseProject.ivyFile
     try {
       val confs = Array[String]("default")
-      val artifactFilter = FilterHelper.getArtifactTypeFilter(Array[String]("xml", "jar", "bundle", "source"))
-      val resolveOptions = new ResolveOptions().setConfs(confs)
-        .setValidate(true)
-        .setArtifactFilter(artifactFilter)
-      val ivy = Ivy.newInstance
-      val settings = ivy.getSettings
 
-      settings.addAllVariables(System.getProperties)
-      ivy.configure(baseProject.ivySettingsFile)
 
-      val report = ivy.resolve(ivyFile.toURI().toURL(), resolveOptions)
-      val md = report.getModuleDescriptor
+      val ivy = {
+        val ivy = Ivy.newInstance
+        ivy.getSettings.addAllVariables(System.getProperties)
+        ivy.configure(baseProject.ivySettingsFile)
+        ivy
+      }
 
-      import scala.collection.JavaConversions._
 
-      val po = new PublishOptions()
-                    .setConfs(confs).setOverwrite(true)
-                    .setPubrevision(version)
-                    .setPubdate(new Date())
+      val moduleRevisionID = {
+        val resolveOptions = new ResolveOptions().setConfs(confs)
+          .setValidate(true)
+          .setArtifactFilter(FilterHelper.getArtifactTypeFilter(Array[String]("xml", "jar", "bundle", "source")))
+        val report = ivy.resolve(ivyFile.toURI().toURL(), resolveOptions)
+        val md = report.getModuleDescriptor
+        md.getModuleRevisionId()
+      }
+
+      val publishOptions = new PublishOptions()
+                            .setConfs(confs).setOverwrite(true)
+                            .setPubrevision(version)
+                            .setPubdate(new Date())
 
       val srcArtifactPattern = List(
         baseProject.publishLocalDir.getAbsolutePath + "/[type]s/pom.xml",
@@ -56,10 +61,10 @@ case class PublishTask(baseProject : BaseProject, resolverName : String, version
 
 
       ivy.publish(
-        md.getModuleRevisionId(),
+        moduleRevisionID,
         srcArtifactPattern,
         resolverName,
-        po)
+        publishOptions)
 
       DefaultTaskResult(this, true, sw)
     }
