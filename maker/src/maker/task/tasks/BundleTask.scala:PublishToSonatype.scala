ed2f1f172babe86aae10b3a17be0b78ed4900d03
Alex.McGuire <alex.mcguire@trafigura.com>
2015-01-29 16:32:28 +0000

    Can publish testReporter to Sonatype

--- maker/src/maker/task/tasks/BundleTask.scala:PublishToSonatype.scala
+++ maker/src/maker/task/tasks/BundleTask.scala:PublishToSonatype.scala
@@ -8,35 +8,106 @@ import java.io.File
 import maker.utils.os.{Command, CommandOutputHandler}
 import maker.utils.FileUtils._
 import spray.json._
+import DefaultJsonProtocol._
+import com.sun.xml.internal.txw2.Content
+import xsbti.Exit
+import scala.collection.immutable.Nil
+import scala.xml.XML
 
 
 case class PublishToSonatype(baseProject : BaseProject, version : String) extends Task with EitherUtils{
   type ErrorMessage = String
   type JsonResponse = String
+  type StagingRepo = String
   def name = s"Publish $baseProject to Sonatype"
 
   def upstreamTasks = 
     PublishLocalTask(baseProject, baseProject.allUpstreamModules, version, signArtifacts = true) :: Nil
 
   def exec(results : Iterable[TaskResult], sw : Stopwatch) = {
-    val repo = FileUtils.withTempDir{
+    FileUtils.withTempDir{
       dir => 
         for{
           bundleJar <- makeBundle(dir)
-          stagingRepoJson <- uploadToSonatype(bundleJar)
+          stagingRepo <- uploadToSonatype(bundleJar)
+          _ <- waitTillRepoIsClosed(stagingRepo)
         } yield{
-          println(s"Returning $stagingRepoJson")
-          stagingRepoJson
+          releaseStagingRepo(dir, stagingRepo)
         }
+    } match {
+      case Left(error) => 
+        DefaultTaskResult(this, false, sw, message = Some(error))
+      case Right(output) => 
+        println(output)
+        DefaultTaskResult(this, true, sw)
     }
-    println(s"repo = $repo")
-    DefaultTaskResult(this, true, sw)
   }
 
-  private def releaseStagingRepo(stagingRepoJson : String) : Either[ErrorMessage, Unit] = {
-    val json = 
-    null
+  case class RepoUris(repositoryUris : Seq[String]){
+    require(repositoryUris.size == 1, s"expected a single uri, got $repositoryUris")
+    def repoIdentifier = repositoryUris(0).split('/').last
   }
+
+  case class PayloadData(stagedRepositoryIds : Seq[String], description : String)
+  case class ReleasePayload(data : PayloadData)
+  implicit val jsonReposFormatter = jsonFormat1(RepoUris)
+  implicit val payloadDataFormatter = jsonFormat2(PayloadData)
+  implicit val releasePayloadFormatter = jsonFormat1(ReleasePayload)
+
+  private def releaseStagingRepo(tmpDir : File, stagingRepo : StagingRepo) : Either[ErrorMessage, Unit] = {
+    val payload = ReleasePayload(PayloadData(List(stagingRepo), ""))
+    val payloadJson = payload.toJson.compactPrint
+    val cmd = Command(
+      CommandOutputHandler(),
+      None,
+      true,
+      "curl", 
+      "-u", "cage433:zoltan",
+      "-o", "out.txt",
+      "-w", "%{http_code}",
+      "-H", "Accept: application/json", 
+      "-H", "Content-Type: application/json",
+      "-d", payloadJson,
+      "https://oss.sonatype.org/service/local/staging/bulk/promote"
+    ).withSavedOutput
+    println(cmd)
+    if (cmd.exec != 0)
+      Left("Exit code was " + cmd.savedOutput)
+    else{
+      println(s"Saved output ${cmd.savedOutput}")
+      Right(cmd.savedOutput)
+    }
+  }
+
+  private def waitTillRepoIsClosed(stagingRepo : String) : Either[ErrorMessage, Unit] = {
+    var isClosed = false
+    var numTriesLeft = 10
+    while(!isClosed && numTriesLeft > 0){
+      println("Trying to see if repo closed")
+      val cmd = Command(
+        CommandOutputHandler.NULL,
+        None,
+        false,
+        "curl",
+        "-u", "cage433:zoltan",
+        s"https://oss.sonatype.org/service/local/staging/repository/$stagingRepo"
+      ).withSavedOutput
+      cmd.exec
+      println(s"***${cmd.savedOutput}****")
+      val xml = XML.loadString(cmd.savedOutput)
+      val status = (xml \ "type").text
+      println(s"status = $status")
+      if (status == "closed")
+        isClosed = true
+      numTriesLeft -= 1
+      Thread.sleep(5000)
+    }
+    if (isClosed)
+      Right(())
+    else 
+      Left("Couldn't close")
+  }
+
   private def uploadToSonatype(bundle : File) : Either[ErrorMessage, JsonResponse] = {
     val cmd = Command(
       CommandOutputHandler.NULL,
@@ -48,12 +119,9 @@ case class PublishToSonatype(baseProject : BaseProject, version : String) extend
       "https://oss.sonatype.org/service/local/staging/bundle_upload"
     ).withSavedOutput
 
-    println(s"cmd = $cmd")
     if (cmd.exec == 0){
-      println(s"Right ${cmd.savedOutput}")
-      Right(cmd.savedOutput)
+      Right(JsonParser(cmd.savedOutput).convertTo[RepoUris].repoIdentifier)
     } else{
-      println(s"Left ${cmd.savedOutput}")
       Left(cmd.savedOutput)
     }
   }
