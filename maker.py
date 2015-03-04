ef7b446287752b0831777d6712e58c21f0c2e34d
Alex McGuire <cage433@gmail.com>
2015-03-04 07:49:23 +0000

    Upgrade to 2.1-SNAPSHOT

--- maker.py
+++ maker.py
@@ -14,7 +14,7 @@ from subprocess import call
 from glob import glob
 
 # Computer generated section
-MAKER_VERSION       = "2.0-SNAPSHOT"
+MAKER_VERSION       = "2.1-SNAPSHOT"
 MAKER_SCALA_VERSION = "2.10.4"
 SONATYPE            = "http://oss.sonatype.org/content/repositories/snapshots/"
 MAVEN               = "http://repo1.maven.org/maven2/"
