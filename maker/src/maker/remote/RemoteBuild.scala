65f5b86b96b49b64a865f782980e0040e1554fa4
cage433 <cage433@gmail.com>
2015-07-10 11:58:03 +0100

    rsync command

--- maker/src/maker/remote/RemoteBuild.scala
+++ maker/src/maker/remote/RemoteBuild.scala
@@ -3,8 +3,21 @@ package maker.remote
 import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import java.net.Socket
 
-object RemoteBuild{
+case class RemoteBuild(remoteHost : String, remotePort : Int, remoteAbsDir : String){
   def remoteCompile(){
+    val copyScalaFilesCommand = Command(
+      "rsync", 
+      "-aive", 
+      "ssh -p 5555", 
+      "--exclude=.git", 
+      "--include=*/", 
+      "--exclude=target*/", 
+      "--include=*.scala",
+      "--exclude=*", 
+      "127.0.0.1",
+      ":/"home",/"alex",/"repos",/"evcel", "evcel"
+    )
+
     val socket = new Socket("localhost", 5555)
     val in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
     val out = new PrintWriter(socket.getOutputStream(), true)
