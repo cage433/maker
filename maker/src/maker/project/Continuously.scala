1d23f8772aec041f301abb6feae5e26856714013
Alex McGuire <cage433@gmail.com>
2015-10-20 12:37:26 -0400

    Extract methods from ProjectTrait

---
+++ maker/src/maker/project/Continuously.scala
@@ -0,0 +1,53 @@
+package maker.project
+
+import maker.task.BuildResult
+import maker.task.compile.{CompilePhase, SourceCompilePhase}
+import maker.utils.FileUtils
+import java.io.File
+
+object Continuously {
+  def apply(project: ProjectTrait, build: () => BuildResult) {
+    var lastTaskTime :Option[Long] = None
+
+    def allSourceFiles : Seq[File] = project.upstreamModules.flatMap(_.sourceFiles(SourceCompilePhase)) ++: 
+    (project.upstreamModules.flatMap{module => module +: module.testModuleDependencies}).distinct.flatMap{module => 
+      CompilePhase.TEST_PHASES.flatMap(module.sourceFiles(_))
+    }
+
+    def sourceFileCount : Int = allSourceFiles.size
+    var lastFileCount : Int = sourceFileCount 
+    def sourceFileNames : String = allSourceFiles.map(_.getPath).sortWith(_<_).mkString(" ")
+    var lastSourceFileNames : String = sourceFileNames
+
+    def printWaitingMessage = println("\nWaiting for source file changes (press 'enter' to interrupt)")
+    def rerunTask{
+      println((build()))
+      lastTaskTime = Some(System.currentTimeMillis)
+      lastFileCount = sourceFileCount
+      lastSourceFileNames = sourceFileNames
+      printWaitingMessage
+    }
+
+
+    def lastSrcModificationTime : Option[Long] = {
+      FileUtils.lastModifiedFileTime(allSourceFiles)
+      //allSourceFiles.map(FileUtils.lastModifiedFileTime(watchedFiles)).max
+      //allUpstreamTestModules.map(proj => {
+          //val watchedFiles = proj.compilePhase.sourceFiles ++ proj.testCompilePhase.sourceFiles
+          //FileUtils.lastModifiedFileTime(watchedFiles)
+        //}).max
+    }
+    printWaitingMessage
+    while (true) {
+      Thread.sleep(1000)
+      if (System.in.available > 0 && System.in.read == 10) return
+      (lastTaskTime,  lastSrcModificationTime, lastFileCount, sourceFileCount, lastSourceFileNames, sourceFileNames) match {
+        case (None, _, _, _, _, _) => { rerunTask }                        // Task has never been run
+        case (Some(t1), Some(t2), _, _, _, _) if t1 < t2 => { rerunTask }  // Code has changed since task last run
+        case (_, _, m, n, _, _) if m != n => { rerunTask }                  // Source file has been added or deleted
+        case (_, _, _, _, a, b) if a != b => { rerunTask }                  // Source file has been renamed
+        case _ =>                                                    // Either no code yet or code has not changed
+      }
+    }
+  }
+}
---
+++ maker/src/maker/project/VimClasspath.scala
@@ -0,0 +1,37 @@
+package maker.project
+
+import maker.task.compile.{CompilePhase, TestCompilePhase}
+import java.io.File
+import maker.utils.FileUtils
+
+object VimClasspath extends FileUtils {
+  /**
+    * To run tests from Vim it is convenient to have _all_ test classes on the classpath,
+    * Not just those modules on whom we have a test dependency
+    */
+  def writeVimClasspath(project: ProjectTrait) {
+    var components = CompilePhase.PHASES.flatMap{phase => 
+      project.compilationTargetDirectories(phase :: Nil)
+    }.distinct ++: project.resourceDirectories(CompilePhase.PHASES)
+    components ++= project.dependencyJars(TestCompilePhase)
+    components ++= project.unmanagedLibs
+    components ++= project.managedLibDir :: Nil
+    components = components.filter{
+      file =>
+        file.exists && (!file.isDirectory || file.list.size > 0)
+    }
+    var relativePaths = components.map(_.relativeTo(file("."))).map(_.getPath)
+    relativePaths = relativePaths.map{
+      path => 
+        if (path.endsWith(".jar")) {
+          val pathComponents = path.split(File.separator)
+          (pathComponents.dropRight(1) :+ "*").mkString(File.separator)
+        } else 
+          path
+    }.distinct
+    val cp = relativePaths.mkString(File.pathSeparator)
+    val cpFile : File = file("maker-classpath.txt")
+    println(s"Writing classpath to file $cpFile")
+    writeToFile(cpFile, cp)
+  }
+}
